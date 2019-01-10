c9c871213c432a61bc9b925351e1f91a37d4f35c
Nikolas Nyby <nikolas@gnu.org>
2019-01-09 23:04:27 -0500

    Enable eslint, and do some cleanup
    Hi,
    So do with this what you want. I just noticed the bookreader doesn't
    have eslint set up. This gets BookReader.js passing under eslint.
    I've also added a travis.yml file if you want to have travis
    automatically check that.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    }
+};
