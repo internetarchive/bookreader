2a515e194576080726991ed458ef92179f2f7a8d
Shubham Kumar <shubhamkrai123@gmail.com>
2019-06-27 07:29:49 +0530

    added linter integration into CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
     "env": {
         "browser": true,
-        "jquery": true
+        "es6": true,
+        "node": true
     },
     "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
     "parserOptions": {
-        "ecmaVersion": 5
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
     }
 };
