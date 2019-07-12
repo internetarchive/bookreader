dd9ec59229ff86b26c0a750376c53432972d9834
Shubham Kumar <shubhamkrai123@gmail.com>
2019-07-12 23:32:40 +0530

    added custom test

--- tests/bookreader.js
+++ tests/bookreader.js
@@ -1,8 +1,1 @@
-require("../BookReader/jquery-1.10.1.js");
-require("../BookReader/jquery-ui-1.12.0.min.js");
-require("../BookReader/jquery.browser.min.js");
-require("../BookReader/dragscrollable-br.js");
-require("../BookReader/jquery.colorbox-min.js");
-require("../BookReader/jquery.bt.min.js");
-
 var br = require("../BookReader/BookReader.js");
