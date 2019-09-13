0e8e411bf71bc8c1c67de833693bc57d6da1b2c2
Drini Cami <cdrini@gmail.com>
2019-09-13 17:35:41 -0400

    [129/webpack] webpack debug maps + easier to extend to new files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'production',
-    entry: './src/js/plugins/tts/plugin.tts.js',
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
-    },
-    output: {
-        filename: 'plugin.tts.js',
-        path: path.resolve(__dirname, 'BookReader/plugins')
-    }
-};
+module.exports = [
+    buildJsFromTo({from: 'plugins/tts/plugin.tts.js', to: 'BookReader/plugins/plugin.tts.js'})
+];
+
+/**
+ * @param {Object} opts
+ * @param {String} opts.from
+ * @param {String} opts.to
+ */
+function buildJsFromTo({ from: srcEntryFile, to: outputFile }) {
+    return {
+        mode: 'production',
+        entry: './' + path.join('src/js/', srcEntryFile),
+        module: {
+            rules: [
+                { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            ]
+        },
+    
+        output: {
+            filename: path.basename(outputFile),
+            path: path.resolve(__dirname, path.parse(outputFile).dir)
+        },
+    
+        // Accurate source maps at the expense of build time.
+        // The source map is intentionally exposed
+        // to users via sourceMapFilename for prod debugging.
+        devtool: 'source-map'
+    };
+}
