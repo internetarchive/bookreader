07e3a72ba61189e2ee0b7a8816af14d2395ba4d2
Internet Archive <archive@internets-air-4.lan>
2019-10-02 21:19:26 -0700

    unit test

---
+++ tests/plugins/fullscreen_menu_toggle/plugin.fullscreen_menu_toggle.test.js
@@ -0,0 +1,32 @@
+require('../../../BookReader/BookReader.js');
+require('../../../src/js/plugins/fullscreen_menu_toggle/plugin.fullscreen_menu_toggle.js');
+
+let binder;
+beforeEach(() => {
+    document.querySelector = jest.fn(() => true);
+    document.body.innerHTML = '<div id="BookReader">';
+
+    BookReader.prototype.initToolbar = jest.fn();
+    BookReader.prototype.initNavbar = jest.fn();
+    BookReader.prototype.prepareOnePageView = jest.fn();
+    BookReader.prototype.enterFullScreen = jest.fn();
+    BookReader.prototype.bindNavigationHandlers = jest.fn();
+
+    binder = jest.spyOn(BookReader.prototype, 'manageFullScreenToggle');
+})
+
+afterEach(() => {
+    jest.clearAllMocks();
+    binder = null;
+})
+
+test('Plugin: Fullscreen Menu Toggle - successfully initializes', () => {
+    expect(BookReader.defaultOptions.enableFullScreenMenuToggle).toEqual(true);
+    expect(BookReader.prototype.manageFullScreenToggle).toBeDefined();
+
+    const bookrdr = new BookReader();
+    bookrdr.init();
+
+    expect(bookrdr.menuFullscreenFadeManager).toBeDefined();
+    expect(binder).toHaveBeenCalled();
+});
