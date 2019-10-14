97ae0415cc1650f82950895ea6f4ddae365242eb
Isa Herico-Velasco <isaherico@gmail.com>
2019-10-14 03:19:26 +0200

    Black background click/tap now toggles menu

--- src/js/plugins/menu_toggle/plugin.menu_toggle.js
+++ src/js/plugins/menu_toggle/plugin.menu_toggle.js
@@ -25,7 +25,7 @@
       removeEventHandlers(br);
     }
   
-    var removeEventHandlers = function removeEventHandlers(br) {
+    var removeEventHandlers = function removeEventHandlers (br) {
       if (br.refs.$brPageViewEl) {
         br.refs.$brPageViewEl[0].removeEventListener('click', toggleMenuIfCenterClick, true);
       }
@@ -33,47 +33,67 @@
         br.refs.$brTwoPageView[0].removeEventListener('click', toggleMenuIfCenterClick, true);
       }
     }
+
+    var toggleNav = function toggleNav (br) {
+      var menuIsShowing = br.navigationIsVisible();
+      if (menuIsShowing) {
+        br.hideNavigation();
+      } else {
+        br.showNavigation();
+      }
+    }
   
     var toggleMenuIfCenterClick = function toggleMenuIfCenterClick(br, e) {
-      var menuManager = br.menuToggle;
-      var bookWidth = e.currentTarget.offsetWidth;
-      var leftOffset = e.currentTarget.offsetLeft
+      var book = e.currentTarget;
+      if (!book) {
+        return;
+      }
+      var bookWidth = book.offsetWidth;
+      var leftOffset = book.offsetLeft
       var bookEndPageFlipArea = Math.round(bookWidth / 3);
       var leftThreshold = Math.round(bookEndPageFlipArea + leftOffset); // without it, the click area is small
       var rightThreshold = Math.round(bookWidth - bookEndPageFlipArea + leftOffset);
       var isCenterClick = (e.clientX > leftThreshold) && (e.clientX < rightThreshold);
       
       if (isCenterClick) {
-        if (menuManager.menuIsShowing) {
-          br.hideNavigation();
-          menuManager.menuIsShowing = false;
-        } else {
-          br.showNavigation();
-          menuManager.menuIsShowing = true;
-        }
+        toggleNav(br);
         event.stopPropagation();
       }
     }
-  
+
     function registerEventHandlers(br) {
+
       var brContainer = document.querySelector('.BRcontainer') || {};
+      var mainBRWrapper = document.querySelector('.BookReader') || {};
       var firstChild = brContainer.firstChild;
   
       if (firstChild) {
-        firstChild.addEventListener('click', toggleMenuIfCenterClick.bind(this, br), true);
+        firstChild.addEventListener('click', function (e) { 
+          toggleMenuIfCenterClick(br, e);
+        }, true);
       }
+
+      var toggleAtBackgroundClick = function toggleAtBackgroundClick (e) {
+        const isBackground = $(event.target).hasClass('BookReader')
+          || $(event.target).hasClass('BRcontainer')
+          || $(event.target).hasClass('BRemptypage')
+          || $(event.target).hasClass('BRpageview');
+        if (isBackground) {
+          toggleNav(br);
+        }
+      };
+      mainBRWrapper.addEventListener('click', toggleAtBackgroundClick, true)
     }
 
     BookReader.prototype.initMenuToggle = function brInitMenuToggle(e) {
-
-  
+      var br = this;
       var menuToggleEventRegister = function menuToggleEventRegister (e) {
-        registerEventHandlers(this);
-      }.bind(this);
+        registerEventHandlers(br);
+      };
 
       var setupDOMandHandlers = function setupDOMandHandlers(e) {
-        setupNavForToggle(this);
-      }.bind(this));
+        setupNavForToggle(br);
+      };
   
       var eventsToHandle = [
         'BookReader:1PageViewSelected',
@@ -87,16 +107,12 @@
       $(window).on('orientationchange', menuToggleEventRegister);
       $(document).on('BookReader:fullscreenToggled', setupDOMandHandlers);
       $(window).on('DOMContentLoaded', setupDOMandHandlers);
+      setupDOMandHandlers();
     };
 
     BookReader.prototype.setup = (function(super_) {
       return function(options) {
         super_.call(this, options);
-        this.menuToggle = {
-          menuIsShowing: true,
-          setupNavForToggle: setupNavForToggle,
-          removeToggleFromNav: removeToggleFromNav,
-        };
       };
     })(BookReader.prototype.setup);
 
