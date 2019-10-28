9adbefc9af566c0f932792ebd0757f730088f032
Isa Herico-Velasco <isaherico@gmail.com>
2019-10-28 16:38:18 -0700

    remove test reference

--- tests/plugins/menu_toggle/plugin.menu_toggle.test.js
+++ tests/plugins/menu_toggle/plugin.menu_toggle.test.js
@@ -5,27 +5,8 @@ let initializeSpy;
 beforeEach(() => {
     document.querySelector = jest.fn(() => true);
     document.body.innerHTML = '<div id="BookReader">';
-
-    BookReader.prototype.initToolbar = jest.fn();
-    BookReader.prototype.initNavbar = jest.fn();
-    BookReader.prototype.prepareOnePageView = jest.fn();
-    BookReader.prototype.enterFullScreen = jest.fn();
-    BookReader.prototype.bindNavigationHandlers = jest.fn();
-
-    initializeSpy = jest.spyOn(BookReader.prototype, 'initMenuToggle');
-})
-
-afterEach(() => {
-    jest.clearAllMocks();
-    initializeSpy = null;
 })
 
 test('Plugin: Fullscreen Menu Toggle - successfully initializes', () => {
     expect(BookReader.defaultOptions.enableMenuToggle).toEqual(true);
-    expect(BookReader.prototype.initMenuToggle).toBeDefined();
-
-    const bookrdr = new BookReader();
-    bookrdr.init();
-
-    expect(initializeSpy).toHaveBeenCalled();
 });
