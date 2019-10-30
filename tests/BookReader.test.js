7eeb8fc6be70eeb235a59f309272471572d4eb33
Isa Herico-Velasco <isaherico@gmail.com>
2019-10-30 12:15:57 -0700

    more tests around registered events

--- tests/BookReader.test.js
+++ tests/BookReader.test.js
@@ -56,3 +56,21 @@ test('testing debounce', () => {
   expect(func).toHaveBeenCalledTimes(1);  // func called
 });
 
+test('has registered PostInit events', () => {
+  expect(BookReader.eventNames.PostInit).toBeTruthy();
+});
+
+test('has registered zoom events', () => {
+  expect(BookReader.eventNames.zoomIn).toBeTruthy();
+  expect(BookReader.eventNames.zoomOut).toBeTruthy();
+});
+
+test('has registered view type selected events', () => {
+  expect(BookReader.eventNames['1PageViewSelected']).toBeTruthy();
+  expect(BookReader.eventNames['2PageViewSelected']).toBeTruthy();
+  expect(BookReader.eventNames['3PageViewSelected']).toBeTruthy();
+});
+
+test('has registered fullscreen toggle event', () => {
+  expect(BookReader.eventNames.fullscreenToggled).toBeTruthy();
+});
