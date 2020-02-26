2dd4f33e9b0bc36fbcccb248f5cde084597cc268
Isa Herico-Velasco <isaherico@gmail.com>
2020-02-25 17:57:08 -0800

    extend e2e initial tests for BookReader to test menu behavior

--- tests/e2e/example.test.js
+++ tests/e2e/example.test.js
@@ -1,9 +1,70 @@
 /* global fixture */
-import { Selector } from 'testcafe';
+import { ClientFunction, Selector } from 'testcafe';
 
 fixture`Tests page interactions`.page('http://localhost:8000/BookReaderDemo/demo-simple.html');
 
-test('Clicking flip right button flips page and updates location', async (t) => {
-  await t.click('.book_right');
-  await t.expect((await Selector('img[src$="page003.jpg"]')).visible).ok();
+test('Nav menu displays properly', async t => {
+  await t.expect((await Selector('.BRfooter')).visible).ok();
+  await t.expect((await Selector('.BRicon.book_left')).visible).ok();
+  await t.expect((await Selector('.BRicon.book_right')).visible).ok();
+  await t.expect((await Selector('.BRicon.onepg')).visible).ok();
+  await t.expect((await Selector('.BRicon.twopg')).visible).ok();
+  await t.expect((await Selector('.BRicon.thumb')).visible).ok();
+  await t.expect((await Selector('.BRicon.zoom_in')).visible).ok();
+  await t.expect((await Selector('.BRicon.zoom_out')).visible).ok();
+  await t.expect((await Selector('.BRicon.full')).visible).ok();
 });
+
+test('Clicking `page flip buttons` changes pages and updates location', async t => {
+  const flipRight = await Selector('.book_right');
+  const flipLeft = await Selector('.book_left');
+  await t.click(flipRight);
+  await t.expect((await Selector('img[src$="page002.jpg"]')).visible).ok();
+  await t.expect((await Selector('img[src$="page003.jpg"]')).visible).ok();
+  await t.click(flipLeft);
+  await t.expect((await Selector('img[src$="page001.jpg"]')).visible).ok();
+  await t.expect((await Selector('img[src$="transparent.png"]')).visible).ok();
+});
+
+test('Clicking `2 page view` brings up 2 pages at a time', async t => {
+  const twoPageViewToggle = await Selector('.BRicon.twopg');
+  await t.click(twoPageViewToggle);
+  const twoPageContainer = await Selector('.BRtwopageview');
+  await t.expect(twoPageContainer.visible).ok();
+  const images = twoPageContainer.find('img.BRpageimage');
+  await t.expect(images.count).eql(2);
+});
+
+test('Clicking `1 page view` brings up 1 at a time', async t => {
+  const onePageViewToggle = await Selector('.BRicon.onepg');
+  await t.click(onePageViewToggle);
+  const onePageViewContainer = await Selector('.BRpageview');
+  await t.expect(onePageViewContainer.visible).ok();
+  const images = onePageViewContainer.find('.BRpagediv1up');
+  // we pre-fetch the page in question & the 2 after it
+  await t.expect(images.count).eql(3);
+});
+
+test('Clicking `thumbnail view` brings up all of the page thumbnails', async t => {
+  const thumbnailViewToggle = await Selector('.BRicon.thumb');
+  await t.click(thumbnailViewToggle);
+  const thumbnailContainer = await Selector('.BRpageview');
+  await t.expect(thumbnailContainer.visible).ok();
+  const images = thumbnailContainer.find('.BRpagedivthumb');
+  await t.expect(images.count).gt(0);
+});
+
+test('Clicking `full screen button` and BookReader fills browser window', async (t) => {
+  const brContainer = await Selector('.BRcontainer');
+  const fullScreenToggle = await Selector('.BRicon.full');
+  const windowWidth = await ClientFunction(() => window.innerWidth)();
+
+  // initial in-page
+  await t.expect(brContainer.getBoundingClientRectProperty('width')).lt(windowWidth);
+  await t.click(fullScreenToggle);
+  // full screen
+  await t.expect(brContainer.getBoundingClientRectProperty('width')).eql(windowWidth);
+  await t.click(fullScreenToggle);
+  // in-page
+  await t.expect(brContainer.getBoundingClientRectProperty('width')).lt(windowWidth);
+})
