77b59b996849f89b7f1b8d289e0561360dcaf819
Neeraj Sharma <neeraj@archive.org>
2020-03-03 18:35:34 +0530

    Added JS docs, Fixed indentation

--- src/js/plugins/plugin.resume.js
+++ src/js/plugins/plugin.resume.js
@@ -1,7 +1,8 @@
+/* global BookReader */
+
 /**
  * Plugin to remember the current page in a cookie
  */
-
 jQuery.extend(BookReader.defaultOptions, {
   enablePageResume: true,
   /**
@@ -12,21 +13,21 @@ jQuery.extend(BookReader.defaultOptions, {
 
 BookReader.prototype.init = (function(super_) {
   return function() {
-      super_.call(this);
-      if (this.options.enablePageResume) {
-          this.bind(BookReader.eventNames.fragmentChange, function() {
-              var params = this.paramsFromCurrent();
-              this.updateResumeValue(params.index);
-          }.bind(this));
-      }
+    super_.call(this);
+    if (this.options.enablePageResume) {
+      this.bind(BookReader.eventNames.fragmentChange, function() {
+        var params = this.paramsFromCurrent();
+        this.updateResumeValue(params.index);
+      }.bind(this));
+    }
   };
 })(BookReader.prototype.init);
 
 /**
-* Get's the page resume value, for remembering reader's page
-* Can be overriden for different implementation
-* @return {Number|null}
-*/
+ * Get's the page resume value, for remembering reader's page
+ * Can be overriden for different implementation
+ * @return {Number|null}
+ */
 BookReader.prototype.getResumeValue = function() {
   var val = BookReader.docCookies.getItem('br-resume');
   if (val !== null) return parseInt(val);
@@ -34,10 +35,12 @@ BookReader.prototype.getResumeValue = function() {
 }
 
 /**
-* Set's the page resume value, for remembering reader's page
-* Can be overriden for different implementation
-* @param {Number} the leaf index
-*/
+ * Set's the page resume value, for remembering reader's page
+ * Can be overriden for different implementation
+ *
+ * @param {Number} index leaf index
+ * @param {String} cookieName
+ */
 BookReader.prototype.updateResumeValue = function(index, cookieName) {
   var ttl = new Date(+new Date + 12096e5); // 2 weeks
   var path = this.options.resumeCookiePath || window.location.pathname;
@@ -45,29 +48,34 @@ BookReader.prototype.updateResumeValue = function(index, cookieName) {
   BookReader.docCookies.setItem(cookieName, index, ttl, path, null, false);
 }
 
-/*\
-|*|  https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
-|*|  https://developer.mozilla.org/User:fusionchess
-|*|  https://github.com/madmurphy/cookies.js
-|*|  This framework is released under the GNU Public License, version 3 or later.
-|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
-\*/
+/**
+ * Use to get, set and remove item from cookie
+ *
+ * See more:
+ *  https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
+ *  https://developer.mozilla.org/User:fusionchess
+ *  https://github.com/madmurphy/cookies.js
+ *  This framework is released under the GNU Public License, version 3 or later.
+ *  http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
 BookReader.docCookies = {
-getItem: function (sKey) {
-  if (!sKey) { return null; }
-  return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-},
-setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-  var sExpires = "";
-  if (vEnd) {
-    sExpires = "; expires=" + vEnd.toUTCString();
-  }
-  document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-  return true;
-},
-removeItem: function (sKey, sPath, sDomain) {
-  if (!this.hasItem(sKey)) { return false; }
-  document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-  return true;
-},
+  getItem: function (sKey) {
+    if (!sKey) { return null; }
+    return decodeURIComponent(
+      // eslint-disable-next-line no-useless-escape
+      document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+  },
+  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+    var sExpires = "";
+    if (vEnd) {
+      sExpires = "; expires=" + vEnd.toUTCString();
+    }
+    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    return true;
+  },
+  removeItem: function (sKey, sPath, sDomain) {
+    if (!this.hasItem(sKey)) { return false; }
+    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+    return true;
+  },
 };
