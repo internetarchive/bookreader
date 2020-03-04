679bf3349a37ab6a1964a1fbb9030618b3652774
Drini Cami <cdrini@gmail.com>
2020-03-04 10:26:41 -0500

    appease eslint for plugin.iframe.js

--- src/js/plugins/plugin.iframe.js
+++ src/js/plugins/plugin.iframe.js
@@ -1,3 +1,4 @@
+/* global BookReader */
 /**
  * Plugin for two-way communication between a BookReader in an IFrame and the
  * parent web page
@@ -5,11 +6,11 @@
 
 const MESSAGE_TYPE_FRAGMENT_CHANGE = 'bookReaderFragmentChange';
 
-BookReader.prototype.init = (function(super_) {
-    return function() {
-        super_.call(this);
-        _attachEventListeners(this);
-    };
+BookReader.prototype.init = (function (super_) {
+  return function () {
+    super_.call(this);
+    _attachEventListeners(this);
+  };
 })(BookReader.prototype.init);
 
 /**
@@ -21,27 +22,27 @@ BookReader.prototype.init = (function(super_) {
  * @param {BookReader} br
  * @param {Window?} [parent]
  */
-export function _attachEventListeners(br, parent=window.parent) {
-    // Not embedded, abort
-    if (!parent) {
-        return;
+export function _attachEventListeners(br, parent = window.parent) {
+  // Not embedded, abort
+  if (!parent) {
+    return;
+  }
+
+  br.bind(BookReader.eventNames.fragmentChange, () => {
+    const fragment = br.fragmentFromParams(br.paramsFromCurrent());
+
+    parent.postMessage(
+      { type: MESSAGE_TYPE_FRAGMENT_CHANGE, fragment },
+      '*'
+    );
+  });
+
+  window.addEventListener('message', event => {
+    // Not a recognized message type, abort
+    if (!event.data || event.data.type !== MESSAGE_TYPE_FRAGMENT_CHANGE) {
+      return;
     }
 
-    br.bind(BookReader.eventNames.fragmentChange, () => {
-        const fragment = br.fragmentFromParams(br.paramsFromCurrent());
-
-        parent.postMessage(
-            { type: MESSAGE_TYPE_FRAGMENT_CHANGE, fragment },
-            '*'
-        );
-    });
-
-    window.addEventListener('message', event => {
-        // Not a recognized message type, abort
-        if (!event.data || event.data.type !== MESSAGE_TYPE_FRAGMENT_CHANGE) {
-            return;
-        }
-
-        br.updateFromParams(br.paramsFromFragment(event.data.fragment));
-    });
+    br.updateFromParams(br.paramsFromFragment(event.data.fragment));
+  });
 }
