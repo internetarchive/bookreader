0f6c448b838e95868f8df893c3b3536119a7a2ad
Isa Herico-Velasco <isaherico@gmail.com>
2020-03-05 06:47:42 -0800

    clean up margins

--- src/js/plugins/plugin.mobile_nav.js
+++ src/js/plugins/plugin.mobile_nav.js
@@ -9,122 +9,122 @@
  */
 
 jQuery.extend(BookReader.defaultOptions, {
-    enableMobileNav: true,
-    mobileNavTitle: 'Internet Archive',
-    mobileNavFullscreenOnly: false,
+  enableMobileNav: true,
+  mobileNavTitle: 'Internet Archive',
+  mobileNavFullscreenOnly: false,
 });
 
 BookReader.prototype.setup = (function(super_) {
-    return function (options) {
-        super_.call(this, options);
+  return function (options) {
+    super_.call(this, options);
 
-        this.enableMobileNav = options.enableMobileNav;
-        this.mobileNavTitle = options.mobileNavTitle;
-        this.mobileNavFullscreenOnly = options.mobileNavFullscreenOnly;
+    this.enableMobileNav = options.enableMobileNav;
+    this.mobileNavTitle = options.mobileNavTitle;
+    this.mobileNavFullscreenOnly = options.mobileNavFullscreenOnly;
 
-        this.refs.$mmenu = null;
-    };
+    this.refs.$mmenu = null;
+  };
 })(BookReader.prototype.setup);
 
 
 // Extend initToolbar
 BookReader.prototype.initToolbar = (function (super_) {
-    return function (mode, ui) {
-        var self = this;
+  return function (mode, ui) {
+    let self = this;
 
-        if (this.enableMobileNav) {
-            var $drawerEl = this.buildMobileDrawerElement();
-            this.refs.$br.append($drawerEl);
+    if (this.enableMobileNav) {
+      let $drawerEl = this.buildMobileDrawerElement();
+      this.refs.$br.append($drawerEl);
 
-            // Render info into mobile info before mmenu
-            this.buildInfoDiv(this.$('.BRmobileInfo'));
-            this.buildShareDiv(this.$('.BRmobileShare'));
+      // Render info into mobile info before mmenu
+      this.buildInfoDiv(this.$('.BRmobileInfo'));
+      this.buildShareDiv(this.$('.BRmobileShare'));
 
-            var $mmenuEl = $drawerEl;
-            $mmenuEl.mmenu({
-              navbars: [
-                 { "position": "top" },
-              ],
-              navbar: {
-                add: true,
-                title: this.mobileNavTitle,
-                titleLink: 'panel'
-              },
-              extensions: [ "panelshadow" ],
-            }, {
-              offCanvas: {
-                wrapPageIfNeeded: false,
-                zposition: 'next',
-                pageSelector: this.el,
-              }
-            });
-
-            var $BRpageviewField = $mmenuEl.find('.BRpageviewValue');
-            $mmenuEl.data('mmenu').bind('opened', function() {
-                // Update "Link to this page view" link
-                if ($BRpageviewField.length)
-                    $BRpageviewField.val(window.location.href);
-            });
-
-            // High contrast button
-            $drawerEl.find('.high-contrast-button').click(function() {
-                self.refs.$br.toggleClass('high-contrast');
-            });
-
-            // Bind mobile switch buttons
-            $drawerEl.find('.DrawerLayoutButton.one_page_mode').click(function() {
-                self.switchMode(self.constMode1up);
-            });
-            $drawerEl.find('.DrawerLayoutButton.two_page_mode').click(function() {
-                self.switchMode(self.constMode2up);
-            });
-            $drawerEl.find('.DrawerLayoutButton.thumbnail_mode').click(function() {
-                self.switchMode(self.constModeThumb);
-            });
-
-            if (this.mobileNavFullscreenOnly) {
-                $(document.body).addClass('BRbodyMobileNavEnabledFullscreen');
-            } else {
-                $(document.body).addClass('BRbodyMobileNavEnabled');
-            }
-
-            this.refs.$mmenu = $mmenuEl;
+      var $mmenuEl = $drawerEl;
+      $mmenuEl.mmenu({
+        navbars: [
+          { "position": "top" },
+        ],
+        navbar: {
+          add: true,
+          title: this.mobileNavTitle,
+          titleLink: 'panel'
+        },
+        extensions: [ "panelshadow" ],
+      }, {
+        offCanvas: {
+          wrapPageIfNeeded: false,
+          zposition: 'next',
+          pageSelector: this.el,
         }
+      });
 
-        // Call the parent method at the end, because it binds events to DOM
-        super_.apply(this, arguments);
+      let $BRpageviewField = $mmenuEl.find('.BRpageviewValue');
+      $mmenuEl.data('mmenu').bind('opened', function() {
+        // Update "Link to this page view" link
+        if ($BRpageviewField.length)
+          $BRpageviewField.val(window.location.href);
+      });
+
+      // High contrast button
+      $drawerEl.find('.high-contrast-button').click(function() {
+        self.refs.$br.toggleClass('high-contrast');
+      });
+
+      // Bind mobile switch buttons
+      $drawerEl.find('.DrawerLayoutButton.one_page_mode').click(function() {
+        self.switchMode(self.constMode1up);
+      });
+      $drawerEl.find('.DrawerLayoutButton.two_page_mode').click(function() {
+        self.switchMode(self.constMode2up);
+      });
+      $drawerEl.find('.DrawerLayoutButton.thumbnail_mode').click(function() {
+        self.switchMode(self.constModeThumb);
+      });
+
+      if (this.mobileNavFullscreenOnly) {
+        $(document.body).addClass('BRbodyMobileNavEnabledFullscreen');
+      } else {
+        $(document.body).addClass('BRbodyMobileNavEnabled');
+      }
+
+      this.refs.$mmenu = $mmenuEl;
+    }
+
+    // Call the parent method at the end, because it binds events to DOM
+    super_.apply(this, arguments);
 
 
-        if (this.enableMobileNav) {
-            // Need to bind more, console after toolbar is initialized
-            this.$('.BRmobileHamburger').click(function() {
-                if ($mmenuEl.data('mmenu').getInstance().vars.opened) {
-                    $mmenuEl.data('mmenu').close();
-                } else {
-                    $mmenuEl.data('mmenu').open();
-                }
-            })
+    if (this.enableMobileNav) {
+      // Need to bind more, console after toolbar is initialized
+      this.$('.BRmobileHamburger').click(function() {
+        if ($mmenuEl.data('mmenu').getInstance().vars.opened) {
+          $mmenuEl.data('mmenu').close();
+        } else {
+          $mmenuEl.data('mmenu').open();
         }
-    };
+      })
+    }
+  };
 })(BookReader.prototype.initToolbar);
 
 
 BookReader.prototype.buildToolbarElement = (function (super_) {
-    return function () {
-        var $el = super_.call(this);
-        if (this.enableMobileNav) {
-            var escapedTitle = BookReader.util.escapeHTML(this.bookTitle);
-            $el
-            .addClass('responsive')
-            .prepend($(
-                   "<span class='BRmobileHamburgerWrapper'>"
+  return function () {
+    let $el = super_.call(this);
+    if (this.enableMobileNav) {
+      let escapedTitle = BookReader.util.escapeHTML(this.bookTitle);
+      $el
+        .addClass('responsive')
+        .prepend($(
+          "<span class='BRmobileHamburgerWrapper'>"
                 +     "<button class=\"BRmobileHamburger\"></button>"
                 +     "<span class=\"BRtoolbarMobileTitle\" title=\""+escapedTitle+"\">" + this.bookTitle + "</span>"
                 +   "</span>"
-            ));
-        }
-        return $el;
-    };
+        ));
+    }
+    return $el;
+  };
 })(BookReader.prototype.buildToolbarElement);
 
 /**
@@ -133,14 +133,14 @@ BookReader.prototype.buildToolbarElement = (function (super_) {
  * @return {jqueryElement}
  */
 BookReader.prototype.buildMobileDrawerElement = function() {
-    var experimentalHtml = '';
-    if (this.enableExperimentalControls) {
-      experimentalHtml += "<div class=\"DrawerSettingsTitle\">Experimental (may not work)</div>"
+  let experimentalHtml = '';
+  if (this.enableExperimentalControls) {
+    experimentalHtml += "<div class=\"DrawerSettingsTitle\">Experimental (may not work)</div>"
         +"        <button class='BRaction default high-contrast-button'>Toggle high contrast</button>";
-    }
+  }
 
-    var $el = $(
-      "<nav id=\"BRmobileMenu\" class=\"BRmobileMenu\">"
+  let $el = $(
+    "<nav id=\"BRmobileMenu\" class=\"BRmobileMenu\">"
       +"  <ul>"
       +"    <li>"
       +"      <span>"
@@ -178,8 +178,8 @@ BookReader.prototype.buildMobileDrawerElement = function() {
       +"    </li>"
       +"  </ul>"
       +"</nav>"
-    );
-    return $el;
+  );
+  return $el;
 };
 
 /**
@@ -187,11 +187,11 @@ BookReader.prototype.buildMobileDrawerElement = function() {
  * include it in the scoped $ function.
  */
 BookReader.prototype.$ = (function (super_) {
-    return function (arg) {
-        var $results = super_.call(this, arg);
-        if (this.refs.$mmenu) {
-            $results = $results.add(this.refs.$mmenu.find(arg));
-        }
-        return $results;
-    };
+  return function (arg) {
+    let $results = super_.call(this, arg);
+    if (this.refs.$mmenu) {
+      $results = $results.add(this.refs.$mmenu.find(arg));
+    }
+    return $results;
+  };
 })(BookReader.prototype.$);
