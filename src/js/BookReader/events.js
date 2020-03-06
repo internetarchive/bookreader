1a3d0bbed2b67519e3c00a2916845391e04f4055
Drini Cami <cdrini@gmail.com>
2020-03-06 11:44:35 -0500

    [es6-bookreader-core/Toolbar] conver to class

---
+++ src/js/BookReader/events.js
@@ -0,0 +1,18 @@
+export const EVENTS = {
+    /** Indicates that the fragment (a serialization of the reader
+     * state) has changed. */
+    fragmentChange: 'fragmentChange',
+    PostInit: 'PostInit',
+    stop: 'stop',
+    resize: 'resize',
+    // nav events:
+    navToggled: 'navToggled',
+    // menu click events
+    fullscreenToggled: 'fullscreenToggled',
+    zoomOut: 'zoomOut',
+    zoomIn: 'zoomIn',
+    '1PageViewSelected': '1PageViewSelected',
+    '2PageViewSelected': '2PageViewSelected',
+    /* currently 3 represents thumbnail view */
+    '3PageViewSelected': '3PageViewSelected',
+};
