2ab8315d4513756c0c55ca95e8aecf23e5d89cf1
Drini Cami <cdrini@gmail.com>
2020-03-13 18:41:48 -0400

    [es6-bookreader-core/Toolbar] fix classes bug + failed tests

--- src/js/BookReader/utils/classes.js
+++ src/js/BookReader/utils/classes.js
@@ -1,34 +1,36 @@
 /**
- * Exposes a function from a property onto the main class, in such a way that if the
- * main class's method is overriden, the property's class is also overriden.
- * WARNING: This modifies the property class' _prototype_! So it can only be called one.
- * (It must be this way, because BookReader.prototype is overriden usually before any instance
- * is created.)
- * @template TFrom The class we'll be getting the method from
+ * Exposes a function from one class (FromClass) to another (ToClass), in such a way
+ * that if the ToClass's method is overriden, the FromClass's method is also overriden.
+ * WARNING: This modifies FromClass' prototype! So FromClasses cannot be shared between
+ * different ToClasses.
+ * @template TFrom the class we'll be getting the method from
  * @template TTo the class we'll be adding the method to
  * @param {new () => TFrom} FromClass the class to get the method from
  * @param {string} fromMethod the method's name in FromClass
- * @param {function(TFrom): TTo} fromTransform how to get the TTo this to use when setting the method on TFrom
+ * @param {function(TFrom): TTo} fromTransform how to get the TTo `this` to use when setting the method on TFrom
  * @param {new () => TTo} ToClass the class to add the method to
- * @param {string} toMethod the name of the method to add to TTo
+ * @param {string} toMethod the name of the method to add to TTo (likely will be equal to fromMethod)
  * @param {function(TTo): TFrom} toTransform how to get the TFrom this to use when calling the new method
  */
 export function exposeTwoWay(FromClass, fromMethod, fromTransform, ToClass, toMethod, toTransform) {
-    const makeWrapper = fn => {
-      return /** @this {TTo} */ function() {
+    // Wrapper function needed to "capture" the current version of fromMethod
+    let wrapper = (fn => {
+      return function () {
         return fn.apply(toTransform(this), arguments);
-      }
-    };
-    let wrapper = makeWrapper(FromClass.prototype[fromMethod]);
+      };
+    })(FromClass.prototype[fromMethod]);
   
     Object.defineProperty(ToClass.prototype, toMethod, {
       get() { return wrapper; },
       set(overrideFn) {
-        // Overrides expect `this` to be BookReader, so bind as such
-        FromClass.prototype[fromMethod] = /** @this {TFrom} */ function() {
-          return overrideFn.apply(fromTransform(this), arguments);
+        // overrideFn expects `this` to be ToClass, so ensure as such
+        // But we can also call this method from FromClass; need to ensure
+        // it's always called with a ToClass
+        FromClass.prototype[fromMethod] = function() {
+          const newThis = this instanceof FromClass ? fromTransform(this) : this;
+          return overrideFn.apply(newThis, arguments);
         };
-        wrapper = makeWrapper(FromClass.prototype[fromMethod]);
+        wrapper = overrideFn;
       }
     });
 }
--- tests/plugins/plugin.mobile_nav.test.js
+++ tests/plugins/plugin.mobile_nav.test.js
@@ -1,5 +1,3 @@
-/* global BookReader */
-import '../../BookReader/jquery-1.10.1.js';
 import '../../BookReader/jquery-1.10.1.js';
 import '../../BookReader/jquery-ui-1.12.0.min.js';
 import '../../BookReader/jquery.browser.min.js';
@@ -9,10 +7,10 @@ import '../../BookReader/jquery.bt.min.js';
 import '../../BookReader/mmenu/dist/js/jquery.mmenu.min.js';
 import '../../BookReader/mmenu/dist/addons/navbars/jquery.mmenu.navbars.min.js';
 
-import '../../BookReader/BookReader.js';
+import BookReader from '../../src/js/BookReader.js';
 import '../../src/js/plugins/plugin.mobile_nav.js';
 
-
+/** @type {BookReader} */
 let br;
 beforeEach(() => {
   document.body.innerHTML = '<div id="BookReader">';
@@ -40,29 +38,33 @@ describe('Plugin: Mobile Nav', () => {
     expect(br.refs).toHaveProperty('$mmenu');
   });
   test('draws toolbar on init', () => {
-    br = new BookReader();
-    br.initToolbar = jest.fn();
+    let callCount = 0;
+    // Since we have a custom setter set in utils/classes.js, we can't
+    // use regular spying; need to "override" the function with a watcher.
+    BookReader.prototype.initToolbar = function(_super) {
+      return function() {
+        callCount++;
+        _super.apply(this, arguments);
+      }
+    }(BookReader.prototype.initToolbar);
+    const br = new BookReader();
     br.init();
-    expect(br.initToolbar).toHaveBeenCalled();
+    expect(callCount).toBe(1);
   });
   test('sets a class on body to signal it has attached', () => {
     expect($(document.body).hasClass('BRbodyMobileNavEnabled')).toEqual(true);
   });
   test('loads the navbar on init', () => {
-    expect($('#BRmobileMenu').length).toBeGreaterThan(0);
-    expect($('#BRmobileMenu').length).toBeLessThan(2);
+    expect($('#BRmobileMenu')).toHaveLength(1)
   });
   test('There is a Settings Section', () => {
-    expect($('.BRmobileMenu__settings').length).toBeGreaterThan(0);
-    expect($('.BRmobileMenu__settings').length).toBeLessThan(2);
+    expect($('.BRmobileMenu__settings')).toHaveLength(1);
   });
   test('There is a More Info Section', () => {
-    expect($('.BRmobileMenu__moreInfoRow').length).toBeGreaterThan(0);
-    expect($('.BRmobileMenu__moreInfoRow').length).toBeLessThan(2);
+    expect($('.BRmobileMenu__moreInfoRow')).toHaveLength(1);
   });
   test('There is a Sharing Section', () => {
-    expect($('.BRmobileMenu__share').length).toBeGreaterThan(0);
-    expect($('.BRmobileMenu__share').length).toBeLessThan(2);
+    expect($('.BRmobileMenu__share')).toHaveLength(1);
   });
   test('clicking on hamburger opens menu', () => {
     expect($('html').hasClass('mm-opened')).toEqual(false);
