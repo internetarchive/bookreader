3a78f6fdb164720c3721c1720699869237a0b32b
Drini Cami <cdrini@gmail.com>
2020-03-13 18:42:34 -0400

    [es6-bookreader-core/Toolbar] fix eslint

--- src/js/BookReader/utils/classes.js
+++ src/js/BookReader/utils/classes.js
@@ -13,24 +13,24 @@
  * @param {function(TTo): TFrom} toTransform how to get the TFrom this to use when calling the new method
  */
 export function exposeTwoWay(FromClass, fromMethod, fromTransform, ToClass, toMethod, toTransform) {
-    // Wrapper function needed to "capture" the current version of fromMethod
-    let wrapper = (fn => {
-      return function () {
-        return fn.apply(toTransform(this), arguments);
+  // Wrapper function needed to "capture" the current version of fromMethod
+  let wrapper = (fn => {
+    return function () {
+      return fn.apply(toTransform(this), arguments);
+    };
+  })(FromClass.prototype[fromMethod]);
+
+  Object.defineProperty(ToClass.prototype, toMethod, {
+    get() { return wrapper; },
+    set(overrideFn) {
+      // overrideFn expects `this` to be ToClass, so ensure as such
+      // But we can also call this method from FromClass; need to ensure
+      // it's always called with a ToClass
+      FromClass.prototype[fromMethod] = function () {
+        const newThis = this instanceof FromClass ? fromTransform(this) : this;
+        return overrideFn.apply(newThis, arguments);
       };
-    })(FromClass.prototype[fromMethod]);
-  
-    Object.defineProperty(ToClass.prototype, toMethod, {
-      get() { return wrapper; },
-      set(overrideFn) {
-        // overrideFn expects `this` to be ToClass, so ensure as such
-        // But we can also call this method from FromClass; need to ensure
-        // it's always called with a ToClass
-        FromClass.prototype[fromMethod] = function() {
-          const newThis = this instanceof FromClass ? fromTransform(this) : this;
-          return overrideFn.apply(newThis, arguments);
-        };
-        wrapper = overrideFn;
-      }
-    });
+      wrapper = overrideFn;
+    }
+  });
 }
