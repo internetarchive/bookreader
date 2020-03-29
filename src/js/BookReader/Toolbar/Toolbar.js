0ee566e6cf6793c1d72ba1eaf2d882009f927945
Sanchit Jain <171220040@nitdelhi.ac.in>
2020-03-30 03:12:27 +0530

    Update Toolbar.js Closes #<250>
    2 button tags were closed with </a> tag

--- src/js/BookReader/Toolbar/Toolbar.js
+++ src/js/BookReader/Toolbar/Toolbar.js
@@ -330,11 +330,11 @@ export class Toolbar {
 }
 
 export function blankInfoDiv() {
-  // FIXME the <button> el is closed with a </a>
+
   return $(`
     <div class="BRfloat BRinfo">
       <div class="BRfloatHead">About this book
-        <button class="floatShut" href="javascript:;" onclick="$.fn.colorbox.close();"><span class="shift">Close</span></a>
+        <button class="floatShut" href="javascript:;" onclick="$.fn.colorbox.close();"><span class="shift">Close</span></button>
       </div>
       <div class="BRfloatBody">
         <div class="BRfloatCover"></div>
@@ -351,12 +351,12 @@ export function blankInfoDiv() {
 }
 
 export function blankShareDiv() {
-  // FIXME the <button> el is closed with a </a>
+
   return $(`
     <div class="BRfloat BRshare">
       <div class="BRfloatHead">
         Share
-        <button class="floatShut" href="javascript:;" onclick="$.fn.colorbox.close();"><span class="shift">Close</span></a>
+        <button class="floatShut" href="javascript:;" onclick="$.fn.colorbox.close();"><span class="shift">Close</span></button>
       </div>
     </div>`);
 }
