8da829ee418d6f10777fdf13da4d82b0450a32df
Shane Riley <shane@shaneriley.com>
2020-03-30 14:54:59 -0400

    Replaces constants file with env variable

--- tests/e2e/autoplay.test.js
+++ tests/e2e/autoplay.test.js
@@ -1,12 +1,12 @@
 import { ClientFunction } from 'testcafe';
 import BookReader from './models/BookReader';
-import { LOCAL_URL } from './constants';
 
+const { BASE_URL } = process.env;
 const getLocationHref = ClientFunction(() => window.location.href.toString());
 const FLIP_SPEED = 1000;
 const FLIP_DELAY = 2000;
 
-fixture `Autoplay plugin`.page `${LOCAL_URL}demo-autoplay.html`;
+fixture `Autoplay plugin`.page `${BASE_URL}demo-autoplay.html`;
 
 test('page auto-advances after allotted flip speed and delay', async t => {
   await t.wait(FLIP_SPEED + FLIP_DELAY);
--- tests/e2e/base.test.js
+++ tests/e2e/base.test.js
@@ -1,15 +1,15 @@
 /* global fixture */
 import { runBaseTests } from './helpers/base';
 import BookReader from './models/BookReader';
-import { LOCAL_URL } from './constants';
 
+const { BASE_URL } = process.env;
 const localPages = [
   'demo-ia-plato.html',
   // 'demo-ia-olivertwist.html'
 ];
 
 localPages.forEach(function(page) {
-  const url = `${LOCAL_URL}${page}`;
+  const url = `${BASE_URL}${page}`;
   fixture `Base Tests for: ${page}`.page `${url}`;
   runBaseTests(new BookReader());
 });
