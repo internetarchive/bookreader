6197e138c3e4defba3996f8852dfdd706491797c
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2020-05-12 07:01:29 +0530

    Trying to supress the error
    Created an empty implementation of window.open

--- tests/BookReader/Toolbar/Toolbar.test.js
+++ tests/BookReader/Toolbar/Toolbar.test.js
@@ -8,16 +8,16 @@ afterEach(() => {
 
 describe('createPopup', () => {
     test('calls window.open', () => {
-        const openSpy = sinon.spy(window, 'open');
+        const openStub = sinon.stub(window, 'open');
         createPopup('openlibrary.org', 100, 100, 'Open Library');
-        expect(openSpy.callCount).toBe(1);
+        expect(openStub.callCount).toBe(1);
     });
 
     test('opens in center', () => {
-        const openSpy = sinon.spy(window, 'open');
+        const openStub = sinon.stub(window, 'open');
         createPopup('openlibrary.org', 100, 100, 'Open Library');
         // jest default window size is 1024x768
-        expect(openSpy.args[0]).toEqual([
+        expect(openStub.args[0]).toEqual([
             'openlibrary.org',
             'Open Library',
             'status=1,width=100,height=100,top=334,left=462'
