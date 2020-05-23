a664fa2eb0f923d3ec64692470f414fcaace73c2
Pyrojet99 <giacomo.cignoni@gmail.com>
2020-05-23 18:26:50 +0200

    Created e2e mobile search succesful and unsuccesful tests Added mobile DOM elements to Navigation.js

---
+++ tests/e2e/helpers/mobileSearch.js
@@ -0,0 +1,79 @@
+import { ClientFunction, RequestMock } from 'testcafe';
+import { SEARCH_INSIDE_URL_RE , mockResponseFound, mockResponseNotFound } from './mockSearch';
+
+export function runMobileSearchTests(br) {
+  const TEST_TEXT_FOUND = 'theory';
+  const TEST_TEXT_NOT_FOUND = 'HopefullyNotFoundLongWord';
+
+  //building mock response  for successful and unsuccessful search
+  const mockFound = RequestMock()
+    .onRequestTo(SEARCH_INSIDE_URL_RE )
+    .respond(mockResponseFound, 202);
+
+  const mockNotFound = RequestMock()
+    .onRequestTo(SEARCH_INSIDE_URL_RE )
+    .respond(mockResponseNotFound, 202);
+
+  test
+    .requestHooks(mockFound)('Mobile search - successful search', async t => {
+      await t.resizeWindowToFitDevice('Sony Xperia Z', {portraitOrientation: true})
+      const { nav } = br;
+      await t.expect(br.shell.visible).ok();
+      await t.expect(br.BRcontainer.visible).ok();
+
+      //opening side menu and search
+      await t.expect(nav.mobile.hamburgerButton.visible).ok();
+      await t.click(nav.mobile.hamburgerButton);
+      await t.expect(nav.mobile.menuSearchButton.visible).ok();
+      await t.click(nav.mobile.menuSearchButton);
+
+      //assuring that the search bar is enabled
+      await t.expect(nav.mobile.searchBox.visible).ok();
+
+      //testing successful search
+      await t
+        .selectText(nav.mobile.searchBox.child('.BRsearchInput'))
+        .pressKey('delete');
+      await t.typeText(nav.mobile.searchBox.child('.BRsearchInput'), TEST_TEXT_FOUND);
+      await t.click((nav.mobile.searchBox).child('.BRsearchSubmit'));
+      await t.expect(nav.mobile.searchResult.exists).ok();
+      await t.expect(nav.mobile.searchResultText.exists).ok();
+      await t.expect(nav.mobile.searchResultText.innerText).contains(TEST_TEXT_FOUND);
+
+      //checking url
+      const getPageUrl = ClientFunction(() => window.location.href.toString());
+      await t.expect(getPageUrl()).contains(TEST_TEXT_FOUND);
+      await t.maximizeWindow();
+    });
+
+  test
+    .requestHooks(mockNotFound)('Mobile search - unsuccessful search', async t => {
+      await t.resizeWindowToFitDevice('Sony Xperia Z', {portraitOrientation: true})
+      const { nav } = br;
+      await t.expect(br.shell.visible).ok();
+      await t.expect(br.BRcontainer.visible).ok();
+
+      //opening side menu and search
+      await t.expect(nav.mobile.hamburgerButton.visible).ok();
+      await t.click(nav.mobile.hamburgerButton);
+      await t.expect(nav.mobile.menuSearchButton.visible).ok();
+      await t.click(nav.mobile.menuSearchButton);
+
+      //assuring that the search bar is enabled
+      await t.expect(nav.mobile.searchBox.visible).ok();
+
+      //testing unsuccessful search
+      await t
+        .selectText(nav.mobile.searchBox.child('.BRsearchInput'))
+        .pressKey('delete');
+      await t.typeText(nav.mobile.searchBox.child('.BRsearchInput'), TEST_TEXT_NOT_FOUND);
+      await t.click((nav.mobile.searchBox).child('.BRsearchSubmit'));
+      await t.expect(nav.mobile.searchResultText.withText(TEST_TEXT_NOT_FOUND).exists).notOk();
+
+      //checking url
+      const getPageUrl = ClientFunction(() => window.location.href.toString());
+      await t.expect(getPageUrl()).contains(TEST_TEXT_NOT_FOUND);
+      await t.maximizeWindow();
+    });
+
+}
