efe762d2b04c88ff67e97713c77dcf3603913998
Giacomo Cignoni <giacomo.cignoni@gmail.com>
2020-06-06 00:16:41 +0200

    320/bug/autoplay e2e fails (#344)
    * Fixed autoplay too short wait time caused unexpected failed tests
    
    * Updated wait time

--- tests/e2e/autoplay.test.js
+++ tests/e2e/autoplay.test.js
@@ -3,11 +3,11 @@ import { ClientFunction } from 'testcafe';
 const { BASE_URL } = process.env;
 const getLocationHref = ClientFunction(() => window.location.href.toString());
 const FLIP_SPEED = 1000;
-const FLIP_DELAY = 2000;
+const FIRST_PAGE_DELAY = 2000;
 
 fixture `Autoplay plugin`.page `${BASE_URL}demo-autoplay.html`;
 
 test('page auto-advances after allotted flip speed and delay', async t => {
-  await t.wait(FLIP_SPEED + FLIP_DELAY);
+  await t.wait(2 * FLIP_SPEED + FIRST_PAGE_DELAY);
   await t.expect(getLocationHref()).match(/page\/n3/);
 });
