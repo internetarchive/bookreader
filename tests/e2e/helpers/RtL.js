8fda6c7a565b7daab160c2b191783f92f308e719
Pyrojet99 <giacomo.cignoni@gmail.com>
2020-06-08 17:21:27 +0200

    added check flipping page

--- tests/e2e/helpers/RtL.js
+++ tests/e2e/helpers/RtL.js
@@ -1,8 +1,28 @@
+import { ClientFunction } from 'testcafe';
+const getPageUrl = ClientFunction(() => window.location.href);
+
+
 export function runRightToLeftTests (br) {
-  test('Right to Left - correct initialization in two-page view', async t => {
+  test.only('Right to Left - correct initialization in two-page view', async t => {
     const { nav, BRcontainer} = br;
     await t.click(nav.desktop.mode2Up);
-    await t.expect(br.BRcontainer.find('.BRleafEdgeR').getStyleProperty('width')).eql('0px')
+    await t.expect(BRcontainer.find('.BRleafEdgeR').getStyleProperty('width')).eql('0px');
+    const rightPage = BRcontainer.find('.BRpagecontainer.BRemptypage.pagediv-1');
+    const leftPage = BRcontainer.find('.BRpagecontainer.pagediv0');
+    const leftPageLDistance = leftPage.getBoundingClientRectProperty('left');
+    await t.expect(rightPage.getBoundingClientRectProperty('left')).gt(leftPageLDistance);
+    //await t.expect(leftPageLDistance).typeOf("number");
+
+
+
+
+  });
+
+  test.only('Right to Left - assuring flipping left goes to next page', async t => {
+    const { nav } = br;
+    await t.click(nav.desktop.mode2Up);
+    await t.click(nav.desktop.goPrevious);
+    await t.expect(getPageUrl()).match(/page\/n2/);
 
   });
 }
