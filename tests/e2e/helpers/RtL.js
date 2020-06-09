986792ce1b5fec4a16e0b8a568df1415a0fcec73
Pyrojet99 <giacomo.cignoni@gmail.com>
2020-06-09 20:35:31 +0200

    fixed empty page at the start must be on left side Added check for correct bottom slider positioning

--- tests/e2e/helpers/RtL.js
+++ tests/e2e/helpers/RtL.js
@@ -1,28 +1,29 @@
 import { ClientFunction } from 'testcafe';
 const getPageUrl = ClientFunction(() => window.location.href);
 
-
 export function runRightToLeftTests (br) {
-  test.only('Right to Left - correct initialization in two-page view', async t => {
+  test('Right to Left - correct initialization in two-page view', async t => {
     const { nav, BRcontainer} = br;
     await t.click(nav.desktop.mode2Up);
+
+    //checking right leaf edge has 0 width
     await t.expect(BRcontainer.find('.BRleafEdgeR').getStyleProperty('width')).eql('0px');
-    const rightPage = BRcontainer.find('.BRpagecontainer.BRemptypage.pagediv-1');
+
+    //cehcking empty page before the cover is more to the left than the first page
+    const rightEmptyPage = BRcontainer.find('.BRpagecontainer.BRemptypage.pagediv-1');
     const leftPage = BRcontainer.find('.BRpagecontainer.pagediv0');
     const leftPageLDistance = leftPage.getBoundingClientRectProperty('left');
-    await t.expect(rightPage.getBoundingClientRectProperty('left')).gt(leftPageLDistance);
-    //await t.expect(leftPageLDistance).typeOf("number");
+    const rightPageLDistance = rightEmptyPage.getBoundingClientRectProperty('left');    
+    await t.expect(rightPageLDistance).gt(await leftPageLDistance);
 
-
-
-
+    //checks slider is in correct position
+    await t.expect(nav.desktop.sliderRange.getStyleProperty('width')).eql('0px');
   });
 
-  test.only('Right to Left - assuring flipping left goes to next page', async t => {
+  test('Right to Left - assuring flipping left goes to next page', async t => {
     const { nav } = br;
     await t.click(nav.desktop.mode2Up);
     await t.click(nav.desktop.goPrevious);
     await t.expect(getPageUrl()).match(/page\/n2/);
-
   });
 }
