e9560d073253973da93d2067a641f00b25a58abe
Pyrojet99 <giacomo.cignoni@gmail.com>
2020-06-24 15:13:17 +0200

    Added unit tests for mobile chapters

--- tests/plugins/plugin.chapters.test.js
+++ tests/plugins/plugin.chapters.test.js
@@ -8,17 +8,62 @@ import '../../BookReader/jquery.bt.min.js';
 import BookReader from '../../src/js/BookReader.js';
 import '../../src/js/plugins/plugin.chapters.js';
 
+const SAMPLE_TOC = [{
+  "pagenum": "3",
+  "level": 1,
+  "label": "CHAPTER I",
+  "type": {"key": "/type/toc_item"},
+  "title": "THE COUNTRY AND THE MISSION 1",
+  "pageIndex": 3,
+},{
+  "pagenum": "17",
+  "level": 1,
+  "label": "CHAPTER II",
+  "type": {"key": "/type/toc_item"},
+  "title": "THE COUNTRY AND THE MISSION 2",
+  "pageIndex": 17,
+},
+{
+"pagenum": "undefined",
+  "level": 1,
+  "label": "CHAPTER III",
+  "type": {"key": "/type/toc_item"},
+  "title": "THE COUNTRY AND THE MISSION 3",
+  "pageIndex": undefined,
+},
+{
+  "pagenum": "40",
+  "level": 1,
+  "label": "CHAPTER IV",
+  "type": {"key": "/type/toc_item"},
+  "title": "THE COUNTRY AND THE MISSION 4",
+  "pageIndex": 40,
+}];
+
+const SAMPLE_TOC_UNDEF = [
+  {
+    "pagenum": "undefined",
+      "level": 1,
+      "label": "CHAPTER I",
+      "type": {"key": "/type/toc_item"},
+      "title": "THE COUNTRY AND THE MISSION 1",
+      "pageIndex": undefined,
+  },
+  {
+    "pagenum": "undefined",
+      "level": 1,
+      "label": "CHAPTER II",
+      "type": {"key": "/type/toc_item"},
+      "title": "THE COUNTRY AND THE MISSION 2",
+      "pageIndex": undefined,
+  }
+]
+
 let br;
 beforeEach(() => {
   $.ajax = jest.fn().mockImplementation((args) => {
     return Promise.resolve([{
-      table_of_contents: [{
-        "pagenum": "17",
-        "level": 1,
-        "label": "CHAPTER I",
-        "type": {"key": "/type/toc_item"},
-        "title": "THE COUNTRY AND THE MISSION"
-      }]
+      table_of_contents: SAMPLE_TOC,
     }]);
   });
   document.body.innerHTML = '<div id="BookReader">';
@@ -50,4 +95,43 @@ describe('Plugin: Menu Toggle', () => {
     br.init();
     expect($.ajax).toHaveBeenCalled();
   });
+});
+
+
+// Chapter 1, page: 3
+// Chapter 2, page: undefined
+// Chapter 3, page: 15
+
+
+describe('updateTOCState', () => {
+
+  test('Test page is one of TOC', () => {
+    br.init();
+    br.updateTOCState(20, SAMPLE_TOC);
+    expect($('li.table-contents-el')[1].hasClass('current-chapter'));
+  });
+  
+  test('Only one element has .current-chapter', () => {
+    br.init();
+    br.updateTOCState(9, SAMPLE_TOC);
+    expect($('li.table-contents-el.current-chapter').length).toBe(1);
+  })
+
+  test('No chapter has .current-chapter if current index is < than any chapter index', () => {
+    br.init();
+    br.updateTOCState(1, SAMPLE_TOC);
+    expect($('li.table-contents-el.current-chapter').length).toBe(0);
+  })
+
+  test('if current index == chapter index ', () => {
+    br.init();
+    br.updateTOCState(17, SAMPLE_TOC);
+    expect($('li.table-contents-el')[1].hasClass('current-chapter'));
+  })
+
+  test('No chapter has .current-chapter if all chapter have undefined pageIndex ', () => {
+    br.init();
+    br.updateTOCState(10, SAMPLE_TOC);
+    expect($('li.table-contents-el.current-chapter').length).toBe(0);
+  })
 });
