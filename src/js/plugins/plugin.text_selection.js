da862b00c1a6d46a92b76d81dac0d5d351f6679f
Pyrojet99 <giacomo.cignoni@gmail.com>
2020-07-03 12:11:46 +0200

    added svg viewbox

--- src/js/plugins/plugin.text_selection.js
+++ src/js/plugins/plugin.text_selection.js
@@ -1,6 +1,6 @@
-BookReader.prototype.textSelection = function() {
+BookReader.prototype.textSelection = function () {
   console.log("initializing text-selection");
-        
+
   $.ajax({
     type: "GET",
     // url: "https://ia803103.us.archive.org/14/items/goodytwoshoes00newyiala/goodytwoshoes00newyiala_djvu.xml",
@@ -13,39 +13,51 @@ BookReader.prototype.textSelection = function() {
     },
 
     success: function (response) {
-   const xmlMap = response;
+      const xmlMap = response;
 
-  if(xmlMap != undefined){
-    console.log("get xml succesful");
-    const page1  = $(xmlMap).find("OBJECT")[1];
-    console.log("page found");
-    const XMLwidth = $(page1).attr("width");
-    const currWidth = $('.BRpagecontainer').width();
-    const scaleFactor = currWidth/XMLwidth;
+      if (xmlMap != undefined) {
+        console.log("get xml succesful");
+        const page1 = $(xmlMap).find("OBJECT")[1];
+        console.log("page found");
+        const XMLwidth = $(page1).attr("width");
+        const XMLheight = $(page1).attr("height");
 
-    $(page1).find("WORD").each( (i, el) => {
-      const coords = $(el).attr("coords").split(',');
-      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      const textSvg = document.createElementNS("http://www.w3.org/2000/svg", "text");
-      svg.setAttribute("height", scaleFactor*Math.abs(coords[3] - coords[1]));
-      svg.setAttribute("width",  scaleFactor*Math.abs(coords[0] - coords[2]));
-      // svg.setAttribute("x", coords[0]);
-      // svg.setAttribute("y", coords[1]);
-      // const xmlDPI = 500;
-      // const currentDPI = window.devicePixelRatio*96;
-      // console.log(currentDPI)
-      // const scaleFactor = currentDPI/xmlDPI;
-      // console.log(scaleFactor);
+        // const currWidth = $('.BRpagecontainer')[0].width();
+        // const scaleFactor = currWidth/XMLwidth;
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("viewBox", "0 0 " + XMLwidth + " " + XMLheight);
+        $('.BRpagecontainer')[0].append(svg);
+        $(svg).css({
+          "width": "100%",
+          "position": "absolute",
+          "height": "100%",
+          "top": "0",
+          "left": "0",
+        });
 
-      $(svg).css({"position": "absolute",
-        "top": scaleFactor*coords[1],
-        "right": scaleFactor*coords[0]});
-        var textNode = document.createTextNode(el.textContent);
-        textSvg.append(textNode);
-        svg.append(textSvg);
-        $('.BRpagecontainer').append(svg);    
-    })
-  }
-}
-});
+
+        $(page1).find("WORD").each((i, el) => {
+          const [left, bottom, right, top] = $(el).attr("coords").split(',');
+          const textSvg = document.createElementNS("http://www.w3.org/2000/svg", "text");
+          // svg.setAttribute("height", scaleFactor*(coords[1] - coords[3]));
+          // svg.setAttribute("width",  scaleFactor*(coords[2] - coords[0]));
+          textSvg.setAttribute("x", left);
+          textSvg.setAttribute("y", bottom);
+          // const xmlDPI = 500;
+          // const currentDPI = window.devicePixelRatio*96;
+          // console.log(currentDPI)
+          // const scaleFactor = currentDPI/xmlDPI;
+          // console.log(scaleFactor);
+
+          $(textSvg).css({
+            "font-size": "50",
+            "fill": "red",
+          });
+          const textNode = document.createTextNode(el.textContent);
+          textSvg.append(textNode);
+          svg.append(textSvg);
+        })
+      }
+    }
+  });
 }
