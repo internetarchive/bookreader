e3394bc4ab8c9359d8883ecc1fd91bd7b63436eb
Pyrojet99 <giacomo.cignoni@gmail.com>
2020-07-13 18:33:05 +0200

    Added async get for djvu.xml file

--- src/js/plugins/plugin.text_selection.js
+++ src/js/plugins/plugin.text_selection.js
@@ -1,93 +1,107 @@
+//@ts-check
 const BookReader = /** @type {typeof import('../BookReader').default} */(window.BookReader);
-let XMLpageArr = undefined;
 
-class BookreaderWithTextSelection extends BookReader {
-  init() {
+class TextSelectionPlugin {
+
+  constructor() {
+    /**@type {PromiseLike<JQuery<HTMLElement>>} */
+    this.djvuPagesPromise = null;
+  }
+
+  /**
+   * @param {string} ocaid
+   */
+  init(ocaid) {
     console.log("initializing text-selection");
-    const OCAID = this.bookId;
-    $.ajax({
+    this.djvuPagesPromise = $.ajax({
       type: "GET",
-      url: `https://cors.archive.org/cors/${OCAID}/${OCAID}_djvu.xml`,
+      url: `https://cors.archive.org/cors/${ocaid}/${ocaid}_djvu.xml`,
       dataType: "xml",
-      async: false,
 
       error: function (e) {
         console.log("XML reading Failed: ", e);
         return undefined;
-      },
+      }
+    }).then(function (response) {
+      const xmlMap = response;
 
-      success: function (response) {
-        const xmlMap = response;
-
-        if (xmlMap != undefined) {
-          console.log("get xml succesful");
-          XMLpageArr = $(xmlMap).find("OBJECT");
-          console.log("xml page array created");
-          // this.checkTextLayer(page1);
-        }
+      if (xmlMap != undefined) {
+        console.log("get xml succesful");
+        console.log("xml page array created");
+        return $(xmlMap).find("OBJECT");
       }
     });
-    super.init();
-
   }
 
   /**
-  * @param {PageModel} page
-  */
+   * @param {number} index
+   * @returns {Promise<HTMLElement>}
+   */
+  async getPageText(index) {
+    return (await this.djvuPagesPromise)[index];
+  }
+
+  /**
+   * @param {number} pageIndex
+   * @param {JQuery} $container
+   */
+  async createTextLayer(pageIndex, $container) {
+    const $svgLayers = $container.find('textSelctionSVG');
+    if (!$svgLayers.length){
+      console.log(`created svg layer for page ${pageIndex}`)
+      const XMLpage = await this.getPageText(pageIndex);
+      const XMLwidth = $(XMLpage).attr("width");
+      const XMLheight = $(XMLpage).attr("height");
+
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      svg.setAttribute("viewBox", "0 0 " + XMLwidth + " " + XMLheight);
+      $container.append(svg);
+      $(svg).addClass('textSelctionSVG');
+      svg.setAttribute('preserveAspectRatio', 'none');
+      $(svg).css({
+        "width": "100%",
+        "position": "absolute",
+        "height": "100%",
+        "top": "0",
+        "left": "0",
+      });
+
+
+      $(XMLpage).find("WORD").each((i, el) => {
+        // eslint-disable-next-line no-unused-vars
+        const [left, bottom, right, top] = $(el).attr("coords").split(',').map(parseFloat);
+        const textSvg = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        textSvg.setAttribute("x", left.toString());
+        textSvg.setAttribute("y", bottom.toString());
+        textSvg.setAttribute("font-size", (bottom - top).toString());
+
+        $(textSvg).css({
+          "fill": "red",
+        });
+        const textNode = document.createTextNode(el.textContent);
+        textSvg.append(textNode);
+        svg.append(textSvg);
+      })
+    }
+  }
+}
+
+class BookreaderWithTextSelection extends BookReader {
+  init() {
+    const OCAID = this.bookId;
+    this.textSelectionPlugin = new TextSelectionPlugin();
+    this.textSelectionPlugin.init(OCAID);
+    super.init();
+  }
+
+  /**
+   * @param {PageModel} page
+   */
   _createPageContainer(page, styles = {}) {
     console.log(`Created Page ${page.index}`);
     const $container = super._createPageContainer(page, styles);
-    this.createTextLayer(page, $container);
+    this.textSelectionPlugin.createTextLayer(page.index, $container);
     return $container;
   }
 }
 window.BookReader = BookreaderWithTextSelection;
-
-BookreaderWithTextSelection.prototype.createTextLayer = function (page, $container){
-  const $svgLayers = $container.find('textSelctionSVG');
-  if (!$svgLayers.length){
-    console.log(`created svg layer for page ${page.index}`)
-    const XMLpage = XMLpageArr[page.index];
-    const XMLwidth = $(XMLpage).attr("width");
-    const XMLheight = $(XMLpage).attr("height");
-
-    // const currWidth = $('.BRpagecontainer')[0].width();
-    // const scaleFactor = currWidth/XMLwidth;
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("viewBox", "0 0 " + XMLwidth + " " + XMLheight);
-    $container.append(svg);
-    $(svg).addClass('textSelctionSVG')
-    $(svg).css({
-      "width": "100%",
-      "position": "absolute",
-      "height": "100%",
-      "top": "0",
-      "left": "0",
-    });
-
-
-    $(XMLpage).find("WORD").each((i, el) => {
-      const [left, bottom, right, top] = $(el).attr("coords").split(',');
-      const textSvg = document.createElementNS("http://www.w3.org/2000/svg", "text");
-      // svg.setAttribute("height", scaleFactor*(coords[1] - coords[3]));
-      // svg.setAttribute("width",  scaleFactor*(coords[2] - coords[0]));
-      textSvg.setAttribute("x", left);
-      textSvg.setAttribute("y", bottom);
-      textSvg.setAttribute("font-size", bottom - top);
-
-      // const xmlDPI = 500;
-      // const currentDPI = window.devicePixelRatio*96;
-      // console.log(currentDPI)
-      // const scaleFactor = currentDPI/xmlDPI;
-      // console.log(scaleFactor);
-
-      $(textSvg).css({
-      // "font-size": "50",
-        "fill": "red",
-      });
-      const textNode = document.createTextNode(el.textContent);
-      textSvg.append(textNode);
-      svg.append(textSvg);
-    })
-  }
-}
