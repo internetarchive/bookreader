1b6402a262038112847413fa50cd62bd21ce6aea
Shane Riley <shane@shaneriley.com>
2020-07-16 16:28:41 -0400

    Styles range slider

--- static/new_chrome.js
+++ static/new_chrome.js
@@ -2,10 +2,24 @@ const scrubber = document.querySelector('.bookreader [type=range]');
 const buttons = document.querySelectorAll('.bookreader nav button');
 const playButton = document.querySelector('.bookreader [name=play]');
 const readAloudButton = document.querySelector('.bookreader [name="read-aloud"]');
+const webkitRangeFill = document.querySelector('#webkit-fill');
+
+const setWebkitRangeFill = (percent) => {
+  webkitRangeFill.innerHTML = `
+    .color-fill {
+      background: linear-gradient(to right,
+        var(--trackFillColor, #ccc) 0%, var(--trackFillColor, #ccc) ${percent}%,
+        var(--trackColor, #666) ${percent}%, var(--trackColor, #666) 100%);
+    }
+  `;
+};
 
 scrubber.addEventListener('input', () => {
-  scrubber.nextElementSibling.innerText = `${scrubber.value}/${scrubber.max}`;
+  scrubber.parentElement.nextElementSibling.innerText = `${scrubber.value}/${scrubber.max}`;
+  setWebkitRangeFill((scrubber.value / scrubber.max) * 100);
 });
+
+setWebkitRangeFill((scrubber.value / scrubber.max) * 100);
 
 document.querySelector('.bookreader .controls').addEventListener('click', (e) => {
   const parent = e.target.parentElement;
