82229dfd7a648abb2cd3cda1fb5863812d2d98b9
suraj kumar <studmuffinat18@gmail.com>
2020-09-26 19:30:25 +0530

    added eslint comma dangle only multiline

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "comma-dangle": ["error", "only-multiline"],
         "indent": ["error", 2],
         "no-console": "off", // Used too often behind `debug` options; not dealing with this now
         "no-empty": ["error", { "allowEmptyCatch": true }],
