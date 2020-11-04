420b793c98c22afcda5061a89ecaddbf3e9dff27
Drini Cami <cdrini@gmail.com>
2020-11-03 19:12:44 -0500

    Replace jquery-ui export with js import

--- src/js/BookReader/Mode2Up.js
+++ src/js/BookReader/Mode2Up.js
@@ -1,4 +1,6 @@
 // @ts-check
+// effect.js gives acces to extra easing function (e.g. easeInSine)
+import 'jquery-ui/ui/effect.js';
 import '../dragscrollable-br.js';
 import { clamp } from './utils.js';
 import { EVENTS } from './events.js';
