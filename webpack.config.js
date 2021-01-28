652b7b71cd60d6402b47f132bcf83be801b93d0a
clau <dualcnhq@gmail.com>
2021-01-29 07:39:10 +0800

    Fix webpack config and update package deps

--- webpack.config.js
+++ webpack.config.js
@@ -32,21 +32,6 @@ module.exports = [
       // BookReader
       "BookReader.js": "./src/js/BookReader.js",
 
-<<<<<<< HEAD
-      // Plugins (sorted!)
-      'plugins/plugin.archive_analytics.js': { import: './src/js/plugins/plugin.archive_analytics.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.autoplay.js': { import: './src/js/plugins/plugin.autoplay.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.chapters.js': { import: './src/js/plugins/plugin.chapters.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.iframe.js': { import: './src/js/plugins/plugin.iframe.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.menu_toggle.js': { import: './src/js/plugins/menu_toggle/plugin.menu_toggle.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.mobile_nav.js': { import: './src/js/plugins/plugin.mobile_nav.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.resume.js': { import: './src/js/plugins/plugin.resume.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.search.js': { import: './src/js/plugins/search/plugin.search.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.text_selection.js': { import: './src/js/plugins/plugin.text_selection.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.tts.js': { import: './src/js/plugins/tts/plugin.tts.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.url.js': { import: './src/js/plugins/plugin.url.js', dependOn: 'BookReader.js' },
-      'plugins/plugin.vendor-fullscreen.js': { import: './src/js/plugins/plugin.vendor-fullscreen.js', dependOn: 'BookReader.js' }
-=======
       "BookReaderTemplate.js": "./src/BookReaderTemplate/BookReaderTemplate.js",
 
       // Plugins (sorted!)
@@ -98,7 +83,6 @@ module.exports = [
         import: "./src/js/plugins/plugin.vendor-fullscreen.js",
         dependOn: "BookReader.js",
       },
->>>>>>> 00bc292... Rearrange folders and update demo HTML template
     },
 
     externals: {
