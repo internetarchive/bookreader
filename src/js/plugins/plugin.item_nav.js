eb26710f60dc8f666dd9efffe050b227e6c4a560
clau <dualcnhq@gmail.com>
2021-02-18 10:22:17 +0800

    Add some documentation

--- src/js/plugins/plugin.item_nav.js
+++ src/js/plugins/plugin.item_nav.js
@@ -1,3 +1,6 @@
+/**
+ * plugin to load BookReaderTemplate
+ */
 import { BookReaderTemplate } from "../../BookReaderTemplate/template";
 
 window.customElements.define('bookreader-template', BookReaderTemplate);
