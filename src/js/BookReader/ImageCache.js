50a85b55756ec4af42cbd6ba4042cef4b8846383
Drini Cami <cdrini@gmail.com>
2021-04-02 18:13:02 -0400

    Add more types to ImageCache

--- src/js/BookReader/ImageCache.js
+++ src/js/BookReader/ImageCache.js
@@ -1,11 +1,19 @@
+// @ts-check
 /**
  * Creates an image cache dictionary
  * storing images in `<img>` tags so that
  * BookReader can leverage browser caching
  */
+/** @typedef {import("./BookModel").PageIndex} PageIndex */
+/** @typedef {import("../BookReader").default} BookReader */
+
 export class ImageCache {
+  /**
+   * @param {BookReader} br
+   */
   constructor(br) {
     this.br = br;
+    /** @type {{ [index: number]: { uri: string, srcSet: string, reduce: number, loaded: boolean }}} */
     this.cache = {};
     this.defaultScale = 8;
   }
@@ -15,9 +23,8 @@ export class ImageCache {
    * Checks cache first if image is available & of equal/better scale,
    * if not, a new image gets created
    *
-   * @param {String|Number} index - page index
+   * @param {PageIndex} index
    * @param {Number} reduce
-   * @returns {Element} $image
    */
   image(index, reduce) {
     const $thisImage = this.cache[index];
@@ -31,7 +38,7 @@ export class ImageCache {
 
   /**
    * Checks if image has been loaded
-   * @param {String|Number} index - page index
+   * @param {import("./BookModel").PageIndex} index
    * @param {Number} reduce
    * @returns {Boolean}
    */
@@ -50,7 +57,7 @@ export class ImageCache {
    * Removes image from cache
    * Empties `src` & `srcSet` attributes prior to cancel pending requests
    *
-   * @param {String|Number} index - page index
+   * @param {PageIndex} index
    */
   _bustImageCache(index) {
     delete this.cache[index];
@@ -62,9 +69,8 @@ export class ImageCache {
    * - Use only to create an image as it will
    *    bust cache if requested index has one stashed
    *
-   * @param {String|Number} index - page index
+   * @param {PageIndex} index
    * @param {Number} reduce
-   * @returns $image
    */
   _createImage(index, reduce) {
     const hasCache = this.cache[index];
@@ -72,7 +78,7 @@ export class ImageCache {
       this._bustImageCache(index);
     }
     const src = this.br._getPageURI(index, reduce);
-    const srcSet = this.br.options.useSrcSet ? this.br._getPageURISrcset(index, reduce) : [];
+    const srcSet = this.br.options.useSrcSet ? this.br._getPageURISrcset(index, reduce) : '';
     this.cache[index] = { reduce, uri: src, srcSet, loaded: false }
     return this._serveImageElement(index);
   }
@@ -81,13 +87,13 @@ export class ImageCache {
    * @private
    * Generates an image element on the fly from image info in cache
    *
-   * @param {String|Number} index - page index
-   * @returns {Element} jQuery <img> element with base image classes
+   * @param {PageIndex} index
+   * @returns {JQuery<HTMLImageElement>} with base image classes
    */
   _serveImageElement(index) {
     const { uri, srcSet, reduce } = this.cache[index];
 
-    const $img = $('<img />',{
+    const $img = $('<img />', {
       'class': 'BRpageimage',
       'alt': 'Book page image',
       src: uri,
@@ -95,6 +101,7 @@ export class ImageCache {
     })
       .attr('style', '')
       .data('reduce', reduce)
+      // FIXME this has a typo; should be .on('load'); .load is throwing a type error
       .load(() => this.cache[index].loaded = true);
     return $img;
   }
