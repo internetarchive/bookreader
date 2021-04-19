3bdd6e1172ffe8c7d981303aa9294361dcb29dbf
Isa Herico-Velasco <isaherico@gmail.com>
2021-04-19 16:04:13 -0700

    update demo to reflect parent container styling

--- src/BookReaderComponent/BookReaderComponent.js
+++ src/BookReaderComponent/BookReaderComponent.js
@@ -8,48 +8,77 @@ import '../ItemNavigator/ItemNavigator.js'
 import '../BookNavigator/BookNavigator.js'
 
 export class BookReader extends LitElement {
-
   static get properties() {
     return {
       base64Json: { type: String },
+      baseHost: { type: String },
     };
   }
 
-  static get styles() {
-    const black = css`#000`;
-    const white = css`#fff`;
-    const grey222 = css`#222`;
-    const grey333 = css`#333`;
-    const grey999 = css`#999`;
-    const primaryCTAFill = css`#194880`;
-    const primaryCTABorder = css`#c5d1df`;
-    const secondaryCTAFill = grey333;
-    const secondaryCTABorder = grey999;
-    const primaryErrorCTAFill = css`#e51c26`;
-    const primaryErrorCTABorder = css`#f8c6c8`;
+  constructor() {
+    super();
+    this.base64Json = '';
+    this.baseHost = 'https://archive.org';
+  }
 
+  firstUpdated() {
+    this.fetchData();
+  }
+
+  /**
+   * Fetch metadata response from public metadata API
+   * convert response to base64 data
+   * set base64 data to props
+   */
+  async fetchData() {
+    const ocaid = new URLSearchParams(location.search).get('ocaid');
+    const response = await fetch(`${this.baseHost}/metadata/${ocaid}`);
+    const bookMetadata = await response.json();
+    const jsonBtoa = btoa(JSON.stringify(bookMetadata));
+    this.setBaseJSON(jsonBtoa);
+  }
+
+  /**
+   * Set base64 data to prop
+   * @param {string} value - base64 string format
+   */
+  setBaseJSON(value) {
+    this.base64Json = value;
+  }
+
+  render() {
+    return html`
+      <div class="ia-bookreader">
+        <item-navigator
+          itemType="bookreader"
+          basehost=${this.baseHost}
+          item=${this.base64Json}>
+          <div slot="bookreader">
+            <slot name="bookreader"></slot>
+          </div>
+        </item-navigator>
+      </div>
+    `;
+  }
+
+  static get styles() {
     return css`
       :host {
         display: block;
-        --white: ${white};
-        --blackColor: ${black};
-        --primaryBGColor: ${black};
-        --secondaryBGColor: ${grey222};
-        --tertiaryBGColor: ${grey333};
-        --primaryTextColor: var(--white);
-        --primaryCTAFill: ${primaryCTAFill};
-        --primaryCTABorder: ${primaryCTABorder};
-        --secondaryCTAFill: ${secondaryCTAFill};
-        --secondaryCTABorder: ${secondaryCTABorder};
-        --primaryErrorCTAFill: ${primaryErrorCTAFill};
-        --primaryErrorCTABorder: ${primaryErrorCTABorder};
-        --redBookmarkColor: #eb3223;
-        --blueBookmarkColor: #0023f5;
-        --greenBookmarkColor: #75ef4c;
+        --primaryBGColor: var(--black, #000);
+        --secondaryBGColor: #222;
+        --tertiaryBGColor: #333;
+        --primaryTextColor: var(--white, #fff);
+        --primaryCTAFill: #194880;
+        --primaryCTABorder: #c5d1df;
+        --secondaryCTAFill: #333;
+        --secondaryCTABorder: #999;
+        --primaryErrorCTAFill: #e51c26;
+        --primaryErrorCTABorder: #f8c6c8;
       }
 
-      .book-reader {
-        background-color: ${black};
+      .ia-bookreader {
+        background-color: var(----primaryBGColor);
         position: relative;
         width: 100vw;
         height: auto;
@@ -76,51 +105,6 @@ export class BookReader extends LitElement {
         --activityIndicatorLoadingDotColor: var(--primaryTextColor);
         --activityIndicatorLoadingRingColor: var(--primaryTextColor);
       }
-    `;
-  }
-
-  constructor() {
-    super();
-    this.base64Json = '';
-  }
-
-  firstUpdated() {
-    this.fetchData();
-  }
-
-  /**
-   * Fetch metadata response from public metadata API
-   * convert response to base64 data
-   * set base64 data to props
-   */
-  async fetchData() {
-    const ocaid = new URLSearchParams(location.search).get('ocaid');
-    const response = await fetch(`https://archive.org/metadata/${ocaid}`);
-    const bookMetadata = await response.json();
-    const jsonBtoa = btoa(JSON.stringify(bookMetadata));
-    this.setBaseJSON(jsonBtoa);
-  }
-
-  /**
-   * Set base64 data to prop
-   * @param {string} value - base64 string format
-   */
-  setBaseJSON(value) {
-    this.base64Json = value;
-  }
-
-  render() {
-    return html`
-      <div class="book-reader">
-        <item-navigator
-          itemType="bookreader"
-          basehost=${this.baseHost}
-          item=${this.base64Json}>
-          <div slot="bookreader">
-            <slot name="bookreader"></slot>
-          </div>
-        </item-navigator>
-      </div>
     `;
   }
 }
