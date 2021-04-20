9b05f81a16b7d6c0b4a85c8d1d9558b346410e80
Isa Herico-Velasco <isaherico@gmail.com>
2021-04-20 08:54:34 -0700

    book downloads - consolidate css

--- src/iaux-book-downloads/ia-book-downloads.js
+++ src/iaux-book-downloads/ia-book-downloads.js
@@ -3,36 +3,6 @@ import { nothing } from 'lit-html';
 import bookDownloadsCSS from './styles/ia-book-downloads.js';
 
 export class IABookDownloads extends LitElement {
-  static get styles() {
-    const main = css`
-      :host {
-        display: block;
-        height: 100%;
-        padding: 1.5rem 0;
-        overflow-y: auto;
-        font-size: 1.4rem;
-        box-sizing: border-box;
-      }
-
-      a.close ia-icon {
-        --iconWidth: 18px;
-        --iconHeight: 18px;
-      }
-
-      .button {
-        color: var(--primaryTextColor);
-        background: var(--primaryCTAFill);
-        border: 1px solid var(--primaryCTABorder);
-      }
-
-      .button.external {
-        background: var(--secondaryCTAFill, transparent);
-        border: 1px solid var(--secondaryCTABorder, #999);
-      }
-    `;
-    return [main, bookDownloadsCSS];
-  }
-
   static get properties() {
     return {
       downloads: { type: Array },
@@ -90,5 +60,91 @@ export class IABookDownloads extends LitElement {
       <p>To access downloaded books, you need Adobe-compliant software on your device. The Internet Archive will administer this loan, but Adobe may also collect some information.</p>
       <a class="button external" href="https://www.adobe.com/solutions/ebook/digital-editions/download.html" rel="noopener noreferrer" target="_blank">Install Adobe Digital Editions</a>
     `;
+  }
+
+  static get styles() {
+    const main = css`
+      :host {
+        display: block;
+        height: 100%;
+        padding: 1.5rem 0;
+        overflow-y: auto;
+        font-size: 1.4rem;
+        box-sizing: border-box;
+      }
+
+      a.close ia-icon {
+        --iconWidth: 18px;
+        --iconHeight: 18px;
+      }
+      a.close {
+        justify-self: end;
+      }
+
+      .button {
+        color: var(--primaryTextColor);
+        background: var(--primaryCTAFill);
+        border: 1px solid var(--primaryCTABorder);
+        display: inline-block;
+        padding: .6rem 1rem;
+        font-size: 1.4rem;
+        text-decoration: none;
+        text-shadow: 1px 1px #484848;
+        border-radius: 4px;
+      }
+
+      .button.external {
+        background: var(--secondaryCTAFill, transparent);
+        border: 1px solid var(--secondaryCTABorder, #999);
+        text-shadow: none;
+      }
+
+      header {
+        display: flex;
+        align-items: center;
+        padding: 0 2rem;
+      }
+      header p {
+        padding: 0;
+        margin: 0;
+        font-size: 1.2rem;
+        font-weight: bold;
+        font-style: italic;
+      }
+      
+      header div {
+        display: flex;
+        align-items: baseline;
+      }      
+
+      h2 {
+        font-size: 1.6rem;
+      }
+
+      h3 {
+        padding: 0;
+        margin: 0 1rem 0 0;
+        font-size: 1.4rem;
+      }
+
+      ul {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+      }
+
+      p {
+        margin: .3rem 0 0 0;
+      }
+
+      li,
+      ul + p {
+        padding-bottom: 1.2rem;
+        font-size: 1.2rem;
+        line-height: 140%;
+      }
+    `;
+
+    return [main, bookDownloadsCSS];
   }
 }
