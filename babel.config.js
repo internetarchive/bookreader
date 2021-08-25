b8dca7a6fde5af1850af529b0a2c6407f56c0d81
Drini Cami <cdrini@gmail.com>
2021-08-25 17:05:10 +0200

    Fix jest not working with lit-element
    Absolute love to https://github.com/facebook/jest/issues/6229#issuecomment-403539460

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: "> 2%, ie 11, edge 14, samsung > 9, OperaMini all, UCAndroid > 12",
+        useBuiltIns: "usage",
+        corejs: 3
+      }
+    ]
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", {decoratorsBeforeExport: true}],
+    ["@babel/plugin-proposal-class-properties", {loose: true}],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }]
+  ]
+};
