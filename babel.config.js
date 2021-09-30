f204f8d726fb5c12dcc511f7402fb8c19aaeab65
Drini Cami <cdrini@gmail.com>
2021-09-30 13:33:15 -0400

    Make custom elements work on old browsers

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = {
     ["@babel/plugin-proposal-decorators", {decoratorsBeforeExport: true}],
     ["@babel/plugin-proposal-class-properties", {loose: true}],
     ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { loose: true }]
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    "transform-custom-element-classes",
   ]
 };
