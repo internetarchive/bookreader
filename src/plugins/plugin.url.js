62a7172833719cf10c04b43a146163bec00c2e92
Clau Yagyagan <1281581+dualcnhq@users.noreply.github.com>
2021-10-23 05:08:08 +0800

    Add new URL plugin flow

--- src/plugins/plugin.url.js
+++ src/plugins/plugin.url.js
@@ -198,7 +198,11 @@ BookReader.prototype.urlReadHashFragment = function() {
 };
 
 
+<<<<<<< HEAD
 export class UrlPlugin {
+=======
+ export class UrlPlugin {
+>>>>>>> 82bb3226 (Add new URL plugin flow)
 
   constructor(options = {}) {
     this.bookReaderOptions = options;
@@ -219,42 +223,52 @@ export class UrlPlugin {
     this.locationPollId = null;
     this.oldLocationHash = null;
     this.oldUserHash = null;
+
+    this.pullFromAddressBar();
   }
 
   /**
    * Parse JSON object URL state to string format
-   * Arrange path names in an order that it is positioned on the urlSchema
    * @param {object} urlState
    * @returns {string}
    */
   urlStateToUrlString(urlSchema, urlState) {
+    let strPathParams = '';
+    let hasAppendQueryParams = false;
     const searchParams = new URLSearchParams();
-    const pathParams = {};
+
+    const addToSearchParams = (key, value) => {
+      searchParams.append(key, value);
+      hasAppendQueryParams = true;
+    }
+
+    const addToPathParams = (key, value) => {
+      strPathParams = `${strPathParams}/${key}/${value}`;
+    }
 
     Object.keys(urlState).forEach(key => {
-      let schema = urlSchema.find(schema => schema.name === key);
-      if (schema?.deprecated_for) {
-        schema = urlSchema.find(schemaKey => schemaKey.name === schema.deprecated_for);
-      }
-      if (schema?.position == 'path') {
-        pathParams[schema?.name] = urlState[key];
+      const schema = urlSchema.filter(schema => schema.name === key)[0];
+      if (schema) {
+        if (schema.position == 'path') {
+          if (schema.deprecated_for) {
+            addToSearchParams(schema.deprecated_for, urlState[key]);
+          } else {
+            addToPathParams(key, urlState[key]);
+          }
+        } else {
+          addToSearchParams(key, urlState[key]);
+        }
       } else {
-        searchParams.append(schema?.name || key, urlState[key]);
+        addToSearchParams(key, urlState[key]);
       }
     });
 
-    const strPathParams = urlSchema
-      .filter(s => s.position == 'path')
-      .map(schema => pathParams[schema.name] ? `${schema.name}/${pathParams[schema.name]}` : '')
-      .join('/');
-
-    const strStrippedTrailingSlash = `${strPathParams.replace(/\/$/, '')}`;
-    const concatenatedPath = `/${strStrippedTrailingSlash}?${searchParams.toString()}`;
-    return searchParams.toString() ? concatenatedPath : `/${strStrippedTrailingSlash}`;
+    const concatenatedPath = `${strPathParams}?${searchParams.toString()}`;
+    return hasAppendQueryParams ? concatenatedPath : strPathParams;
   }
 
   /**
-   * Parse string URL and add it in the current urlState
+   * Parse string URL add it in the current urlState
    * Example:
    *  /page/n7/mode/2up => {page: 'n7', mode: '2up'}
    *  /page/n7/mode/2up/search/hello => {page: 'n7', mode: '2up', q: 'hello'}
@@ -264,43 +278,27 @@ export class UrlPlugin {
    */
   urlStringToUrlState(urlSchema, str) {
     const urlState = {};
-
+  
     // Fetch searchParams from given {str}
     // Note: whole URL path is needed for URLSearchParams
     const urlPath = new URL(str, 'http://example.com');
     const urlSearchParamsObj = Object.fromEntries(urlPath.searchParams.entries());
-    const urlStrSplitSlashObj = Object.fromEntries(urlPath.pathname
-      .match(/[^\\/]+\/[^\\/]+/g)
-      .map(x => x.split('/'))
-    );
-    const doesKeyExists = (_object, _key) => {
-      return Object.keys(_object).some(value => value == _key);
-    };
-
-    urlSchema
-      .filter(schema => schema.position == 'path')
-      .forEach(schema => {
-        if (!urlStrSplitSlashObj[schema.name] && schema.default) {
-          return urlState[schema.name] = schema.default;
+    const urlStrSplitSlash = urlPath.pathname.split('/');
+    
+    urlSchema.forEach(schema => {
+      const pKey = urlStrSplitSlash.filter(item => item === schema.name);
+      if (pKey.length === 1) {
+        const indexOf = urlStrSplitSlash.indexOf(schema.name) + 1;
+        if (schema.deprecated_for) {
+          urlState[schema.deprecated_for] = urlStrSplitSlash[indexOf];
+        } else {
+          urlState[pKey] = urlStrSplitSlash[indexOf];
         }
-        const hasPropertyKey = doesKeyExists(urlStrSplitSlashObj, schema.name);
-        const hasDeprecatedKey = doesKeyExists(schema, 'deprecated_for') && hasPropertyKey;
-
-        if (hasDeprecatedKey)
-          return urlState[schema.deprecated_for] = urlStrSplitSlashObj[schema.name];
-
-        if (hasPropertyKey)
-          return urlState[schema.name] = urlStrSplitSlashObj[schema.name];
-      });
-
-    // Add searchParams to urlState
-    // Check if Object value is a Boolean and convert value to Boolean
-    // Otherwise, return Object value
-    const isBoolean = value => value === 'true' || (value === 'false' ? false : value);
-    Object.entries(urlSearchParamsObj).forEach(([key, value]) => {
-      urlState[key] = isBoolean(value);
+      }
     });
-
+    Object.keys(urlSearchParamsObj).forEach(params => {
+      urlState[params] = urlSearchParamsObj[params];
+    });
     return urlState;
   }
 
@@ -335,14 +333,13 @@ export class UrlPlugin {
   }
 
   /**
-   * Push URL params to addressbar
+   * Put URL params to addressbar
    */
   pushToAddressBar() {
-    const urlStrPath = this.urlStateToUrlString(this.urlSchema, this.urlState);
+    const urlStrPath = this.urlStateToUrlString(this.urlSchema, this.urlSchema);
     if (this.urlMode == 'history') {
       if (window.history && window.history.replaceState) {
-        const newUrlPath = `${this.urlHistoryBasePath}${urlStrPath}`;
-        window.history.replaceState({}, null, newUrlPath);
+        window.history.replaceState({}, null, urlStrPath);
       }
     } else {
       window.location.replace('#' + urlStrPath);
@@ -351,11 +348,14 @@ export class UrlPlugin {
   }
 
   /**
-   * Get the url and check if it has changed
-   * If it was changeed, update the urlState
+   * @param {string} urlFragment
    */
+  pullFromAddressBar(urlFragment) {
+    this.urlState = this.urlStringToUrlState(this.urlSchema, urlFragment);
+  }
+
   listenForHashChanges() {
-    this.oldLocationHash = window.location.hash.substr(1);
+    this.oldLocationHash = this.urlReadFragment();
     if (this.locationPollId) {
       clearInterval(this.locationPollID);
       this.locationPollId = null;
@@ -363,26 +363,42 @@ export class UrlPlugin {
 
     // check if the URL changes
     const updateHash = () => {
-      const newFragment = window.location.hash.substr(1);
-      const hasFragmentChange = newFragment != this.oldLocationHash;
+      const newFragment = this.urlReadFragment();
+      const hasFragmentChange = (newFragment != this.oldLocationHash) && (newFragment != this.oldUserHash);
 
       if (!hasFragmentChange) { return; }
 
-      this.urlState = this.urlStringToUrlState(newFragment);
+      this.pullFromAddressBar();
+      this.oldUserHash = newFragment;
     };
     this.locationPollId = setInterval(updateHash, 500);
   }
 
   /**
    * Will read either the hash or URL and return the bookreader fragment
-   * @param {string} location
    * @return {string}
    */
-  pullFromAddressBar (location) {
-    const path = this.urlMode === 'history'
-      ? location.substr(this.urlHistoryBasePath.length)
-      : location.substr(1);
-    this.urlState = this.urlStringToUrlState(this.urlSchema, path);
+  urlReadFragment () {
+    if (this.urlMode === 'history') {
+      return window.location.pathname.substr(this.urlHistoryBasePath.length);
+    } else {
+      return window.location.hash.substr(1);
+    }
+  }
+
+  /**
+ * Returns a shortened version of the title with the maximum number of characters
+ * @param {string} bookTitle
+ * @param {number} maximumCharacters
+ * @return {string}
+ */
+  shortTitle (bookTitle, maximumCharacters) {
+    if (bookTitle.length < maximumCharacters) {
+      return bookTitle;
+    }
+
+    const title = `${bookTitle.substr(0, maximumCharacters - 3)}...`;
+    return title;
   }
 
 }
@@ -393,8 +409,10 @@ export class BookreaderUrlPlugin extends BookReader {
     if (this.options.enableUrlPlugin) {
       this.urlPlugin = new UrlPlugin(this.options);
       this.bind(BookReader.eventNames.PostInit, () => {
-        const { urlMode } = this.options;
-
+        const { updateWindowTitle, urlMode } = this.options;
+        if (updateWindowTitle) {
+          document.title = this.urlPlugin.shortTitle(50);
+        }
         if (urlMode === 'hash') {
           this.urlPlugin.listenForHashChanges();
         }
--- tests/jest/plugins/plugin.url.test.js
+++ tests/jest/plugins/plugin.url.test.js
@@ -1,7 +1,6 @@
 import BookReader from '@/src/BookReader.js';
 import '@/src/plugins/plugin.url.js';
 import { UrlPlugin } from '@/src/plugins/plugin.url.js';
-import sinon from 'sinon';
 
 let br;
 beforeAll(() => {
@@ -11,7 +10,6 @@ beforeAll(() => {
 
 afterEach(() => {
   jest.clearAllMocks();
-  sinon.restore();
 });
 
 
@@ -167,6 +165,151 @@ describe.only('UrlPlugin tests', () => {
       const locationUrl = `${window.location.pathname}${window.location.search}`;
       expect(locationUrl).toEqual('/details/foo/page/12/mode/2up?q=hello&view=theater');
     });
+  });
+
+});
+
+describe.only('UrlPlugin', () => {
+  describe('urlStateToUrlString tests', () => {
+    test('urlStateToUrlString with known states in schema', () => {
+      const urlPlugin = new UrlPlugin();
+      const urlSchema = [
+        { name: 'page', position: 'path', default: 'n0' },
+        { name: 'mode', position: 'path', default: '2up' },
+        { name: 'search', position: 'path', deprecated_for: 'q' },
+        { name: 'q', position: 'query_param' },
+        { name: 'sort', position: 'query_param' },
+        { name: 'view', position: 'query_param' },
+        { name: 'admin', position: 'query_param' },
+      ];
+      const urlState = { page: 'n7', mode: '1up' };
+      const urlStateWithQueries = { page: 'n7', mode: '1up', q: 'hello', view: 'theater', sort: 'title_asc' };
+
+      const expectedUrlFromState = '/page/n7/mode/1up';
+      const expectedUrlFromStateWithQueries = '/page/n7/mode/1up?q=hello&view=theater&sort=title_asc';
+
+      expect(urlPlugin.urlStateToUrlString(urlSchema, urlState)).toBe(expectedUrlFromState);
+      expect(urlPlugin.urlStateToUrlString(urlSchema, urlStateWithQueries)).toBe(expectedUrlFromStateWithQueries);
+    });
+
+    test('urlStateToUrlString with unknown states in schema', () => {
+      const urlPlugin = new UrlPlugin();
+      const urlSchema = [
+        { name: 'page', position: 'path', default: 'n0' },
+        { name: 'mode', position: 'path', default: '2up' },
+        { name: 'search', position: 'path', deprecated_for: 'q' },
+        { name: 'q', position: 'query_param' },
+        { name: 'sort', position: 'query_param' },
+        { name: 'view', position: 'query_param' },
+        { name: 'admin', position: 'query_param' },
+      ];
+      const urlState = { page: 'n7', mode: '1up' };
+      const urlStateWithQueries = { page: 'n7', mode: '1up', q: 'hello', viewer: 'theater', sortBy: 'title_asc' };
+
+      const expectedUrlFromState = '/page/n7/mode/1up';
+      const expectedUrlFromStateWithQueries = '/page/n7/mode/1up?q=hello&viewer=theater&sortBy=title_asc';
+
+      expect(urlPlugin.urlStateToUrlString(urlSchema, urlState)).toBe(expectedUrlFromState);
+      expect(urlPlugin.urlStateToUrlString(urlSchema, urlStateWithQueries)).toBe(expectedUrlFromStateWithQueries);
+    });
+  });
+
+  describe('urlStringToUrlState tests', () => {
+    test('urlStringToUrlState without query string', () => {
+      const urlPlugin = new UrlPlugin();
+      const urlSchema = [
+        { name: 'page', position: 'path', default: 'n0' },
+        { name: 'mode', position: 'path', default: '2up' },
+        { name: 'search', position: 'path', deprecated_for: 'q' },
+        { name: 'q', position: 'query_param' },
+        { name: 'sort', position: 'query_param' },
+        { name: 'view', position: 'query_param' },
+        { name: 'admin', position: 'query_param' },
+      ];
+      const url = '/page/n7/mode/2up';
+      const url1 = '/page/n7/mode/1up';
+  
+      expect(urlPlugin.urlStringToUrlState(urlSchema, url)).toEqual({page: 'n7', mode: '2up'});
+      expect(urlPlugin.urlStringToUrlState(urlSchema, url1)).toEqual({page: 'n7', mode: '1up'});
+    });
+  
+    test('urlStringToUrlState with deprecated_for', () => {
+      const urlPlugin = new UrlPlugin();
+      const urlSchema = [
+        { name: 'page', position: 'path', default: 'n0' },
+        { name: 'mode', position: 'path', default: '2up' },
+        { name: 'search', position: 'path', deprecated_for: 'q' },
+        { name: 'q', position: 'query_param' },
+        { name: 'sort', position: 'query_param' },
+        { name: 'view', position: 'query_param' },
+        { name: 'admin', position: 'query_param' },
+      ];
+      const url = '/page/n7/mode/2up/search/hello';
+  
+      expect(urlPlugin.urlStringToUrlState(urlSchema, url)).toEqual({page: 'n7', mode: '2up', q: 'hello'});
+    });
+  
+    test('urlStringToUrlState with query string', () => {
+      const urlPlugin = new UrlPlugin();
+      const urlSchema = [
+        { name: 'page', position: 'path', default: 'n0' },
+        { name: 'mode', position: 'path', default: '2up' },
+        { name: 'search', position: 'path', deprecated_for: 'q' },
+        { name: 'q', position: 'query_param' },
+        { name: 'sort', position: 'query_param' },
+        { name: 'view', position: 'query_param' },
+        { name: 'admin', position: 'query_param' },
+      ];
+      const url = '/page/n7/mode/2up/search/hello?view=theather&foo=bar&sort=title_asc'
+  
+      expect(urlPlugin.urlStringToUrlState(urlSchema, url)).toEqual(
+        {page: 'n7', mode: '2up', q: 'hello', view: 'theather', foo: 'bar', sort: 'title_asc'}
+      );
+    });
+  });
+
+  test('pullFromAddressBar', () => {
+    const urlPlugin = new UrlPlugin();
+    urlPlugin.urlReadFragment = jest.fn(() => '/page/2/mode/1up');
+
+    urlPlugin.pullFromAddressBar(urlPlugin.urlReadFragment());
+    expect(urlPlugin.urlState).toEqual({page: '2', mode: '1up'});
+  });
+
+  test('pushToAddressBar', () => {
+    // TODO
+  });
+
+  test('setUrlParam', () => {
+    const urlPlugin = new UrlPlugin();
+    urlPlugin.setUrlParam('page', '20');
+    urlPlugin.setUrlParam('mode', '2up');
+
+    expect(urlPlugin.urlState).toEqual({page: '20', mode: '2up'});
+  });
+
+  test('removeUrlParam', () => {
+    const urlPlugin = new UrlPlugin();
+    urlPlugin.setUrlParam('page', '20');
+    urlPlugin.setUrlParam('mode', '2up');
+    urlPlugin.removeUrlParam('mode');
+    
+    expect(urlPlugin.urlState).toEqual({page: '20'});
+  });
+
+  test('getUrlParam', () => {
+    const urlPlugin = new UrlPlugin();
+    urlPlugin.setUrlParam('page', '20');
+    urlPlugin.setUrlParam('mode', '2up');
+    expect(urlPlugin.getUrlParam('page')).toEqual('20');
+    expect(urlPlugin.getUrlParam('mode')).toEqual('2up');
+  });
+
+  test('shortTitle', () => {
+    const urlPlugin = new UrlPlugin();
+    
+    expect(urlPlugin.shortTitle('Goody Two-Shoes Book', 10)).toEqual('Goody T...');
+    expect(urlPlugin.shortTitle('Goody Two-Shoes Book', 12)).toEqual('Goody Two...');
   });
 
 });
