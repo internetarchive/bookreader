e1a2dadfeff1a712ec1b011f1cc7dd4cff36df13
Isa Herico-Velasco <isaherico@gmail.com>
2021-12-17 11:16:32 -0800

    unit tests: search provider event hooks

--- tests/karma/BookNavigator/search/search-provider.test.js
+++ tests/karma/BookNavigator/search/search-provider.test.js
@@ -44,10 +44,80 @@ describe('Search Provider', () => {
         onProviderChange: sinon.fake(),
         bookreader: {}
       });
-      sinon.spy(provider, 'onSearchStarted');
-      provider.updateMenu = sinon.fake();
+      sinon.spy(provider, 'updateMenu');
       window.dispatchEvent(new CustomEvent('BookReader:SearchStarted', { detail: { props: { term: 'foo' }}}));
       expect(provider.updateMenu.callCount).to.equal(1);
+    });
+    it('Event: catches `BookReader:SearchCallback`', async() => {
+      const provider = new searchProvider({
+        onProviderChange: sinon.fake(),
+        bookreader: {}
+      });
+      sinon.spy(provider, 'updateMenu');
+      const brStub = {};
+      window.dispatchEvent(new CustomEvent('BookReader:SearchCallback', { detail: { props: { instance: brStub, results: { matches: []} }}}));
+      expect(provider.updateMenu.callCount).to.equal(1);
+      expect(provider.bookreader).to.equal(brStub);
+    });
+    it('Event: catches `BookReader:SearchCallbackEmpty`', async() => {
+      const provider = new searchProvider({
+        onProviderChange: sinon.fake(),
+        bookreader: {}
+      });
+      sinon.spy(provider, 'onSearchRequestError');
+      sinon.spy(provider, 'updateMenu');
+      const brStub = {};
+      window.dispatchEvent(new CustomEvent('BookReader:SearchCallbackEmpty', { detail: { props: { instance: brStub }}}));
+      expect(provider.onSearchRequestError.getCall(0).args[1]).to.equal('noResults');
+      expect(provider.updateMenu.callCount).to.equal(1);
+      expect(provider.bookreader).to.equal(brStub);
+    });
+    it('Event: catches `BookReader:SearchCallbackNotIndexed`', async() => {
+      const provider = new searchProvider({
+        onProviderChange: sinon.fake(),
+        bookreader: {}
+      });
+      const brStub = {};
+      sinon.spy(provider, 'onSearchRequestError');
+      sinon.spy(provider, 'updateMenu');
+      window.dispatchEvent(new CustomEvent('BookReader:SearchCallbackNotIndexed', { detail: { props: { instance: brStub }}}));
+      expect(provider.onSearchRequestError.getCall(0).args[1]).to.equal('notIndexed');
+      expect(provider.updateMenu.callCount).to.equal(1);
+      expect(provider.bookreader).to.equal(brStub);
+    });
+    it('Event: catches `BookReader:SearchCallbackError`', async() => {
+      const provider = new searchProvider({
+        onProviderChange: sinon.fake(),
+        bookreader: {}
+      });
+      sinon.spy(provider, 'onSearchRequestError');
+      sinon.spy(provider, 'updateMenu');
+      const brStub = {};
+      window.dispatchEvent(new CustomEvent('BookReader:SearchCallbackError', { detail: { props: { instance: brStub }}}));
+      expect(provider.onSearchRequestError.getCall(0).args[1]).to.equal(undefined);
+      expect(provider.updateMenu.callCount).to.equal(1);
+      expect(provider.bookreader).to.equal(brStub);
+    });
+    it('Event: catches `component@resultSelected` - user clicks result in side panel - & turns page', async() => {
+      const provider = new searchProvider({
+        onProviderChange: sinon.fake(),
+        bookreader: {
+          leafNumToIndex: sinon.fake(),
+          _searchPluginGoToResult: sinon.fake()
+        }
+      });
+      sinon.spy(provider, 'advanceToPage');
+
+      const searchResultStub = {
+        match: { par: [{ text: 'foo', page: 3 }] },
+      };
+      fixtureSync(provider.component).dispatchEvent(
+        new CustomEvent('resultSelected',
+          { detail: searchResultStub })
+      );
+
+      expect(provider.advanceToPage.callCount).to.equal(1);
+      expect(provider.bookreader._searchPluginGoToResult.callCount).to.equal(1);
     });
   });
 });
