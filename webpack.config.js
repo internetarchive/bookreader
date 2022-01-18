f4aa552847c98250922238252c1ddfe947235b79
Isa Herico-Velasco <isaherico@gmail.com>
2022-01-18 12:57:15 -0800

    update bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = [
       'plugins/plugin.tts.js': { import: './src/plugins/tts/plugin.tts.js', dependOn: 'BookReader.js' },
       'plugins/plugin.url.js': { import: './src/plugins/url/plugin.url.js', dependOn: 'BookReader.js' },
       'plugins/plugin.vendor-fullscreen.js': { import: './src/plugins/plugin.vendor-fullscreen.js', dependOn: 'BookReader.js' },
-      'bookreader-component-bundle.js': { import: './src/BookReaderComponent/BookReaderComponent.js', dependOn: 'BookReader.js' }
+      'ia-bookreader-bundle.js': { import: './src/ia-bookreader/ia-bookreader.js', dependOn: 'BookReader.js' }
     },
 
     externals: {
