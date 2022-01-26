94438976d3b84363156fd877a754b9ec428635c1
Isa Herico-Velasco <isaherico@gmail.com>
2022-01-25 17:37:39 -0800

    use flexbox to propagate container heights/widths

--- src/ia-bookreader/ia-bookreader.js
+++ src/ia-bookreader/ia-bookreader.js
@@ -105,7 +105,7 @@ export class IaBookReader extends LitElement {
 
   render() {
     return html`
-      <div class="ia-bookreader">
+      <div>
         <ia-item-navigator
           ?viewportInFullscreen=${this.fullscreen}
           .basehost=${this.baseHost}
@@ -148,8 +148,6 @@ export class IaBookReader extends LitElement {
     return css`
       :host {
         display: block;
-        height: inherit;
-        min-height: inherit;
         --primaryBGColor: var(--black, #000);
         --secondaryBGColor: #222;
         --tertiaryBGColor: #333;
@@ -160,6 +158,8 @@ export class IaBookReader extends LitElement {
         --secondaryCTABorder: #999;
         --primaryErrorCTAFill: #e51c26;
         --primaryErrorCTABorder: #f8c6c8;
+        background-color: var(--primaryBGColor);
+        position: relative;
       }
 
       :host([fullscreen]),
@@ -170,20 +170,17 @@ export class IaBookReader extends LitElement {
         min-height: unset;
       }
 
-      div[slot="main"],
-      div[slot="main"] > * {
-        height: inherit;
+      div[slot="main"] {
+        display: flex;
+        width: 100%;
+      }
+
+      slot[name="main"] {
+        flex: 1;
       }
 
       slot {
         display: block;
-      }
-
-      .ia-bookreader {
-        background-color: var(--primaryBGColor);
-        position: relative;
-        min-height: inherit;
-        height: inherit;
       }
 
       ia-item-navigator {
