375391ede75f493e5cd6aadfe41cdf79cfc351ba
Isa Herico-Velasco <isaherico@gmail.com>
2022-02-11 22:44:23 -0800

    park needed browsers for support testing

--- .testcaferc.js
+++ .testcaferc.js
@@ -5,6 +5,8 @@ module.exports = {
   browsers: [
     "chrome:headless",
     "firefox:headless",
+    // "browserstack:firefox@52.0:Windows XP",
+    // "browserstack:chrome@78.0:Windows 10",
   ],
   src: ["tests/e2e/**/*test.js"]
 };
