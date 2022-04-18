07e9e49d094717164134bc33f3240dcae6256f27
Drini Cami <cdrini@gmail.com>
2022-04-18 13:32:42 -0400

    Switch rect focus to svg stroke instead of outline (Safari -_-)
    It's not as visually nice because SVG stroke scale from the center as opposed to outwards, but what can you do; you gotta support these fringe browsers! (jk :P) Safari doesn't support rounded corners on outline, and somehow messes up the animation.

--- src/BookReader/PageContainer.js
+++ src/BookReader/PageContainer.js
@@ -101,6 +101,8 @@ export function boxToSVGRect({ l: left, r: right, b: bottom, t: top }) {
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   rect.setAttribute("x", left.toString());
   rect.setAttribute("y", top.toString());
+  rect.setAttribute("rx", "4");
+  rect.setAttribute("ry", "4");
   rect.setAttribute("width", (right - left).toString());
   rect.setAttribute("height", (bottom - top).toString());
   return rect;
