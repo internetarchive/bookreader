c9e87986a75bbd1b0d3c18e7681b79363b6d0d3f
Drini Cami <cdrini@gmail.com>
2022-04-19 15:27:07 -0400

    Feedback from code review

--- src/BookReader/PageContainer.js
+++ src/BookReader/PageContainer.js
@@ -101,10 +101,12 @@ export function boxToSVGRect({ l: left, r: right, b: bottom, t: top }) {
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   rect.setAttribute("x", left.toString());
   rect.setAttribute("y", top.toString());
-  rect.setAttribute("rx", "4");
-  rect.setAttribute("ry", "4");
   rect.setAttribute("width", (right - left).toString());
   rect.setAttribute("height", (bottom - top).toString());
+
+  // Some style; corner radius 4px. Can't set this in CSS yet
+  rect.setAttribute("rx", "4");
+  rect.setAttribute("ry", "4");
   return rect;
 }
 
