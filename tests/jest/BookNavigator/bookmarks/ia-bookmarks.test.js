58945f8f602bfcef24908a207f27ff3de40b8cdd
Drini Cami <cdrini@gmail.com>
2022-05-09 23:26:04 +0000

    Fix karma to jest for ia-bookmarks.test.js

--- tests/jest/BookNavigator/bookmarks/ia-bookmarks.test.js
+++ tests/jest/BookNavigator/bookmarks/ia-bookmarks.test.js
@@ -13,44 +13,33 @@ afterEach(() => {
 
 describe('<ia-bookmarks>', () => {
   test('uses `setup` to start component', async () => {
-    const el = fixtureSync(html`<ia-bookmarks></ia-bookmarks>`);
-    await el.updateComplete;
-
-    let fetchHappened = false;
-    el.bookreader.bookId = 'foo';
-    el.displayMode = 'bookmarks';
-
-    el.fetchBookmarks = async () => {
-      fetchHappened = true;
-      return await Promise.resolve();
+    const el = fixtureSync(html`<ia-bookmarks displayMode="bookmarks"></ia-bookmarks>`);
+    el.bookreader = {
+      bookId: 'foo',
+      $,
+      currentIndex: () => 0,
     };
-
-    const fetchSpy = sinon.spy(el, 'fetchUserBookmarks');
+    await el.updateComplete;
+    const fetchUserBookmarks = sinon.spy(el, 'fetchUserBookmarks');
+    const fetchBookmarks = sinon.stub(el, 'fetchBookmarks');
 
     el.setup();
     await el.updateComplete;
 
-    expect(fetchSpy.callCount).toEqual(1);
-    expect(fetchHappened).toEqual(true);
+    expect(fetchUserBookmarks.callCount).toEqual(1);
+    expect(fetchBookmarks.callCount).toEqual(1);
   });
+
   test('does not fetch user bookmarks if displayMode = login', async () => {
-    const el = fixtureSync(html`<ia-bookmarks></ia-bookmarks>`);
+    const el = fixtureSync(html`<ia-bookmarks displayMode="login"></ia-bookmarks>`);
     await el.updateComplete;
-
-    let fetchHappened = false;
-    el.displayMode = 'login';
-
-    el.fetchBookmarks = async () => {
-      fetchHappened = true;
-      return await Promise.resolve();
-    };
-
     const fetchSpy = sinon.spy(el, 'fetchUserBookmarks');
+    const fetchBookmarks = sinon.stub(el, 'fetchBookmarks');
 
     el.setup();
     await el.updateComplete;
 
     expect(fetchSpy.callCount).toEqual(0);
-    expect(fetchHappened).toEqual(false);
+    expect(fetchBookmarks.callCount).toEqual(0);
   });
 });
