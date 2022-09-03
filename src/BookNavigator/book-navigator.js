675b5fea8e4e9bd3fba06d9be08719802a602700
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-09-02 17:06:44 -0700

    disable contextmenu on restricted books

--- src/BookNavigator/book-navigator.js
+++ src/BookNavigator/book-navigator.js
@@ -443,6 +443,26 @@ export class BookNavigator extends LitElement {
       this.downloadableTypes = downloadURLs;
       this.bookIsRestricted = isRestricted;
     });
+    document.addEventListener('contextmenu', (e) => {
+      if (window.archive_analytics) {
+        window.archive_analytics?.send_event_no_sampling(
+          'BookReader',
+          `contextmenu-${this.bookIsRestricted ? 'restricted' : 'unrestricted'}`,
+          e.target.classList.value
+        );
+      }
+      if (!this.bookIsRestricted) {
+        return;
+      }
+
+      const imagePane = e.target.classList.value.match(/BRscreen|BRpageimage/g);
+      if (!imagePane) {
+        return;
+      }
+
+      e.preventDefault();
+      return false;
+    }, { capture: true });
   }
 
   loadSharedObserver() {
