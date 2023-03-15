f4b7fcae1bc2046ea28e2b54dc24cdbfa15e3390
Drini Cami <cdrini@gmail.com>
2023-03-15 17:20:29 -0400

    Don't error if cookies are blocked

--- tests/jest/util/docCookies.test.js
+++ tests/jest/util/docCookies.test.js
@@ -13,3 +13,12 @@ describe('Helper function: set and get cookie item', () => {
     expect(docCookies.getItem('test-cookie')).toEqual('jack-sparow');
   });
 });
+
+describe('isCookiesBlocked', () => {
+  test('return true if cookies are blocked', () => {
+    expect(docCookies.areCookiesBlocked({ get cookie() { throw new Error(); }})).toBeTruthy();
+  });
+  test('return false if cookies are not blocked', () => {
+    expect(docCookies.areCookiesBlocked({ cookie: 'blah' })).toBeFalsy();
+  });
+});
