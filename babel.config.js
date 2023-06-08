90e75ee9c9a302ce47f6ea28f9427489c328a0f1
Drini Cami <cdrini@gmail.com>
2023-06-08 15:10:12 -0400

    Fix decorator babel step

--- babel.config.js
+++ babel.config.js
@@ -11,9 +11,10 @@ module.exports = {
     ]
   ],
   plugins: [
-    ["@babel/plugin-proposal-decorators", {decoratorsBeforeExport: true}],
-    ["@babel/plugin-proposal-class-properties", {loose: true}],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-decorators", {
+      version: "2018-09",
+      decoratorsBeforeExport: true
+    }],
+    ["@babel/plugin-proposal-class-properties"],
   ]
 };
