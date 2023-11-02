b2bfa99d8bf10889e5d64315ea0ba91c992d6270
Drini Cami <cdrini@gmail.com>
2023-11-02 13:00:28 -0400

    Upgrade to Node 20

--- scripts/preversion.js
+++ scripts/preversion.js
@@ -2,10 +2,6 @@ const { version: OLD_VERSION } = require('../package.json');
 const OLD_RELEASE_URL = `https://api.github.com/repos/internetarchive/bookreader/releases/tags/v${OLD_VERSION}`;
 
 async function main() {
-    // Need this because fetch is ESM-only, and we're on Node 16. Someday we should
-    // be able to move this up to the top without renaming this file to a .mjs or whatever
-    const {default: fetch} = await import('node-fetch');
-
     const {created_at} = await fetch(OLD_RELEASE_URL).then(r => r.json());
     const today = new Date().toISOString().slice(0, -5);
     const searchUrl = 'https://github.com/internetarchive/bookreader/pulls?' + new URLSearchParams({
