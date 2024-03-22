fc5d88777481bfd4d56a1c7f9e7f020e86a9de45
Drini Cami <cdrini@gmail.com>
2024-03-22 16:24:55 -0400

    TMP

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,19 @@ module.exports = {
   presets: [
     [
       "@babel/preset-env",
-      process.env.NODE_ENV == "test" ? { targets: { node: process.version } } :
-        process.env.NODE_ENV == "development" ? { targets: "last 2 Chrome versions, last 2 Firefox versions, last 2 Safari versions, last 2 Edge versions" } : {
-          targets: "> 2%, edge >= 14, samsung > 9, UCAndroid > 12, Safari >= 10",
-          useBuiltIns: "usage",
-          corejs: 3,
+      process.env.BABEL_ENV === "esm" ? {
+        targets: {
+          esmodules: true
         },
-    ],
+        modules: false
+      } :
+        process.env.NODE_ENV == "test" ? { targets: { node: process.version } } :
+          process.env.NODE_ENV == "development" ? { targets: "last 2 Chrome versions, last 2 Firefox versions, last 2 Safari versions, last 2 Edge versions" } : {
+            targets: "> 2%, edge >= 14, samsung > 9, UCAndroid > 12, Safari >= 10",
+            useBuiltIns: "usage",
+            corejs: 3
+          }
+    ]
   ],
   plugins: [
     ["@babel/plugin-proposal-decorators", {
