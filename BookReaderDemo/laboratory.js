a229676ebb9f585ce647d15c72c5a498c10e5799
James S. White <james@jameswhite.org>
2024-06-12 11:15:54 -0500

    see about fetching the json remotely-ish

--- BookReaderDemo/laboratory.js
+++ BookReaderDemo/laboratory.js
@@ -1,57 +1,24 @@
-//
-// This file shows the minimum you need to provide to BookReader to display a book
-//
-// Copyright(c)2008-2009 Internet Archive. Software license AGPL version 3.
+// Import the node-fetch module
+const fetch = require('node-fetch');
+
+// Define the URL of the remote JSON file
+const url = 'https://127.0.0.1:8000/BookReaderDemo/laboratory.json';
 
 // Create the BookReader object
 function instantiateBookReader(selector, extraOptions) {
   selector = selector || '#BookReader';
   extraOptions = extraOptions || {};
-  var options = {
-    ppi: 300,
-    data: [
-      [
-        { width: 2550, height: 3300,
-          uri: '//fapestniegd.xen.prgmr.com/strategic_review/Strv101/0.png' },
-        { width: 2550, height: 3300,
-          uri: '//fapestniegd.xen.prgmr.com/strategic_review/Strv101/1.png' },
-      ],
-      [
-        { width: 2550, height: 3300,
-           uri: '//fapestniegd.xen.prgmr.com/strategic_review/Strv101/2.png' },
-        { width: 2550, height: 3300,
-           uri: '//fapestniegd.xen.prgmr.com/strategic_review/Strv101/3.png' },
-      ],
-      [
-        { width: 2550, height: 3300,
-           uri: '//fapestniegd.xen.prgmr.com/strategic_review/Strv101/4.png' },
-        { width: 2550, height: 3300,
-           uri: '//fapestniegd.xen.prgmr.com/strategic_review/Strv101/5.png' },
-      ]
-    ],
 
-    // Book title and the URL used for the book title link
-    bookTitle: 'BookReader Demo',
-    bookUrl: '../index.html',
-    bookUrlText: 'Back to Demos',
-    bookUrlTitle: 'This is the book URL title',
-
-    // thumbnail is optional, but it is used in the info dialog
-    thumbnail: '//archive.org/download/BookReader/img/page014.jpg',
-    // Metadata is optional, but it is used in the info dialog
-    metadata: [
-      {label: 'Title', value: 'Open Library BookReader Presentation'},
-      {label: 'Author', value: 'Internet Archive'},
-      {label: 'Demo Info', value: 'This demo shows how one could use BookReader with their own content.'},
-    ],
-
-    // Override the path used to find UI images
-    imagesBaseURL: '../BookReader/images/',
-
-    ui: 'full', // embed, full (responsive)
-
-    el: selector,
-  };
+// Fetch the JSON file and parse it into a JavaScript object
+fetch(url)
+  .then(response => response.json())
+  .then(options => {
+    // The 'data' variable contains the parsed JSON as a JavaScript object
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching the JSON file:', error);
+  });
   $.extend(options, extraOptions);
   var br = new BookReader(options);
   br.init();
