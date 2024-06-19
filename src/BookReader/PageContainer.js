8f278643b9a4369b5e57e3c96be5b6fc8ef6e2e0
Drini Cami <cdrini@gmail.com>
2024-06-19 14:13:58 +0200

    Small tweaks to public APIs to facilitate hypothesis support

--- src/BookReader/PageContainer.js
+++ src/BookReader/PageContainer.js
@@ -18,7 +18,10 @@ export class PageContainer {
     this.$container = $('<div />', {
       'class': `BRpagecontainer ${page ? `pagediv${page.index}` : 'BRemptypage'}`,
       css: { position: 'absolute' },
-    }).attr('data-side', page?.pageSide);
+    })
+      .attr('data-side', page?.pageSide)
+      .attr('data-index', page?.index)
+      .attr('data-label', page?.getPageNum());
 
     if (isProtected) {
       this.$container.append($('<div class="BRscreen" />'));
