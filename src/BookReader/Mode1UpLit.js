d5efff16dc94cdd4f04496c0264941eb0a4e9d2f
Jason Buckner <jason@jasonbuckner.com>
2025-01-22 14:07:34 -0800

    Remove customElement decorators

--- src/BookReader/Mode1UpLit.js
+++ src/BookReader/Mode1UpLit.js
@@ -1,10 +1,10 @@
 // @ts-check
-import { customElement, property, query } from 'lit/decorators.js';
-import {LitElement, html} from 'lit';
+import { property, query } from 'lit/decorators.js';
+import { LitElement, html } from 'lit';
 import { styleMap } from 'lit/directives/style-map.js';
 import { ModeSmoothZoom } from './ModeSmoothZoom';
 import { arrChanged, genToArray, sum, throttle } from './utils';
-import { HTMLDimensionsCacher } from "./utils/HTMLDimensionsCacher";
+import { HTMLDimensionsCacher } from './utils/HTMLDimensionsCacher';
 import { ScrollClassAdder } from './utils/ScrollClassAdder';
 import { ModeCoordinateSpace } from './ModeCoordinateSpace';
 /** @typedef {import('./BookModel').BookModel} BookModel */
@@ -17,7 +17,6 @@ import { ModeCoordinateSpace } from './ModeCoordinateSpace';
 // I _have_ to make this globally public, otherwise it won't let me call
 // it's constructor :/
 /** @implements {SmoothZoomable} */
-@customElement('br-mode-1up')
 export class Mode1UpLit extends LitElement {
   /****************************************/
   /************** PROPERTIES **************/
@@ -86,13 +85,15 @@ export class Mode1UpLit extends LitElement {
   get worldStyle() {
     const wToR = this.coordSpace.worldUnitsToRenderedPixels;
     return {
-      width: wToR(this.worldDimensions.width) + "px",
-      height: wToR(this.worldDimensions.height) + "px",
+      width: wToR(this.worldDimensions.width) + 'px',
+      height: wToR(this.worldDimensions.height) + 'px',
     };
   }
 
   /** @type {HTMLElement} */
-  get $container() { return this; }
+  get $container() {
+    return this;
+  }
 
   /** @type {HTMLElement} */
   @query('.br-mode-1up__visible-world')
@@ -128,10 +129,12 @@ export class Mode1UpLit extends LitElement {
     if (smooth) {
       this.style.scrollBehavior = 'smooth';
     }
-    this.scrollTop = this.coordSpace.worldUnitsToVisiblePixels(this.pageTops[index] - this.SPACING_IN / 2);
+    this.scrollTop = this.coordSpace.worldUnitsToVisiblePixels(
+      this.pageTops[index] - this.SPACING_IN / 2,
+    );
     // TODO: Also h center?
     if (smooth) {
-      setTimeout(() => this.style.scrollBehavior = '', 100);
+      setTimeout(() => (this.style.scrollBehavior = ''), 100);
     }
   }
 
@@ -194,7 +197,7 @@ export class Mode1UpLit extends LitElement {
       this.throttledUpdateRenderedPages();
       if (this.visiblePages.length) {
         // unclear why this is ever really happening
-        this.br.displayedIndices = this.visiblePages.map(p => p.index);
+        this.br.displayedIndices = this.visiblePages.map((p) => p.index);
         this.br.updateFirstIndex(this.br.displayedIndices[0]);
         this.br._components.navbar.updateNavIndexThrottled();
       }
@@ -211,7 +214,9 @@ export class Mode1UpLit extends LitElement {
   }
 
   updatePages() {
-    this.pages = genToArray(this.book.pagesIterator({ combineConsecutiveUnviewables: true }));
+    this.pages = genToArray(
+      this.book.pagesIterator({ combineConsecutiveUnviewables: true }),
+    );
   }
 
   /** @override */
@@ -242,50 +247,59 @@ export class Mode1UpLit extends LitElement {
 
   /** @override */
   render() {
-    return html`
-      <div class="br-mode-1up__world" style=${styleMap(this.worldStyle)}></div>
+    return html` <div
+        class="br-mode-1up__world"
+        style=${styleMap(this.worldStyle)}
+      ></div>
       <div class="br-mode-1up__visible-world">
-        ${this.renderedPages.map(p => this.renderPage(p))}
+        ${this.renderedPages.map((p) => this.renderPage(p))}
       </div>`;
   }
 
   /** @param {PageModel} page */
   createPageContainer = (page) => {
-    return this.pageContainerCache[page.index] || (
-      this.pageContainerCache[page.index] = (
+    return (
+      this.pageContainerCache[page.index] ||
+      (this.pageContainerCache[page.index] =
         // @ts-ignore I know it's protected, TS! But Mode1Up and BookReader are friends.
-        this.br._createPageContainer(page.index)
-      )
+        this.br._createPageContainer(page.index))
     );
-  }
+  };
 
   /** @param {PageModel} page */
   renderPage = (page) => {
     const wToR = this.coordSpace.worldUnitsToRenderedPixels;
     const wToV = this.coordSpace.worldUnitsToVisiblePixels;
-    const containerWidth = this.coordSpace.visiblePixelsToWorldUnits(this.htmlDimensionsCacher.clientWidth);
+    const containerWidth = this.coordSpace.visiblePixelsToWorldUnits(
+      this.htmlDimensionsCacher.clientWidth,
+    );
 
     const width = wToR(page.widthInches);
     const height = wToR(page.heightInches);
-    const left = Math.max(this.SPACING_IN, (containerWidth - page.widthInches) / 2);
+    const left = Math.max(
+      this.SPACING_IN,
+      (containerWidth - page.widthInches) / 2,
+    );
     const top = this.pageTops[page.index];
 
     const transform = `translate(${wToR(left)}px, ${wToR(top)}px)`;
-    const pageContainerEl = this.createPageContainer(page)
-      .update({
-        dimensions: {
-          width,
-          height,
-          top: 0,
-          left: 0,
-        },
-        reduce: page.width / wToV(page.widthInches),
-      }).$container[0];
+    const pageContainerEl = this.createPageContainer(page).update({
+      dimensions: {
+        width,
+        height,
+        top: 0,
+        left: 0,
+      },
+      reduce: page.width / wToV(page.widthInches),
+    }).$container[0];
 
     pageContainerEl.style.transform = transform;
-    pageContainerEl.classList.toggle('BRpage-visible', this.visiblePages.includes(page));
+    pageContainerEl.classList.toggle(
+      'BRpage-visible',
+      this.visiblePages.includes(page),
+    );
     return pageContainerEl;
-  }
+  };
 
   /************** VIRTUAL SCROLLING LOGIC **************/
 
@@ -306,7 +320,7 @@ export class Mode1UpLit extends LitElement {
       left: vToW(scrollLeft),
       width: vToW(clientWidth),
     };
-  }
+  };
 
   /**
    * @returns {PageModel[]}
@@ -314,20 +328,26 @@ export class Mode1UpLit extends LitElement {
   computeRenderedPages() {
     // Also render 1 page before/after
     // @ts-ignore TS doesn't understand the filtering out of null values
-    return [
-      this.visiblePages[0]?.prev,
-      ...this.visiblePages,
-      this.visiblePages[this.visiblePages.length - 1]?.next,
-    ]
-      .filter(p => p)
-      // Never render more than 10 pages! Usually means something is wrong
-      .slice(0, 10);
+    return (
+      [
+        this.visiblePages[0]?.prev,
+        ...this.visiblePages,
+        this.visiblePages[this.visiblePages.length - 1]?.next,
+      ]
+        .filter((p) => p)
+        // Never render more than 10 pages! Usually means something is wrong
+        .slice(0, 10)
+    );
   }
 
-  throttledUpdateRenderedPages = throttle(() => {
-    this.renderedPages = this.computeRenderedPages();
-    this.requestUpdate();
-  }, 100, null)
+  throttledUpdateRenderedPages = throttle(
+    () => {
+      this.renderedPages = this.computeRenderedPages();
+      this.requestUpdate();
+    },
+    100,
+    null,
+  );
 
   /**
    * @param {PageModel[]} pages
@@ -350,21 +370,29 @@ export class Mode1UpLit extends LitElement {
    */
   computeDefaultScale(page) {
     // Default to real size if it fits, otherwise default to full width
-    const containerWidthIn = this.coordSpace.renderedPixelsToWorldUnits(this.clientWidth);
-    return Math.min(1, containerWidthIn / (page.widthInches + 2 * this.SPACING_IN)) || 1;
+    const containerWidthIn = this.coordSpace.renderedPixelsToWorldUnits(
+      this.clientWidth,
+    );
+    return (
+      Math.min(
+        1,
+        containerWidthIn / (page.widthInches + 2 * this.SPACING_IN),
+      ) || 1
+    );
   }
 
   computeWorldDimensions() {
     return {
-      width: Math.max(...this.pages.map(p => p.widthInches)) + 2 * this.SPACING_IN,
+      width:
+        Math.max(...this.pages.map((p) => p.widthInches)) + 2 * this.SPACING_IN,
       height:
-          sum(this.pages.map(p => p.heightInches)) +
-          (this.pages.length + 1) * this.SPACING_IN,
+        sum(this.pages.map((p) => p.heightInches)) +
+        (this.pages.length + 1) * this.SPACING_IN,
     };
   }
 
   computeVisiblePages() {
-    return this.pages.filter(page => {
+    return this.pages.filter((page) => {
       const PT = this.pageTops[page.index];
       const PB = PT + page.heightInches;
 
@@ -377,12 +405,14 @@ export class Mode1UpLit extends LitElement {
   /************** INPUT HANDLERS **************/
 
   attachScrollListeners = () => {
-    this.addEventListener("scroll", this.updateVisibleRegion);
+    this.addEventListener('scroll', this.updateVisibleRegion);
     this.scrollClassAdder.attach();
-  }
+  };
 
   detachScrollListeners = () => {
-    this.removeEventListener("scroll", this.updateVisibleRegion);
+    this.removeEventListener('scroll', this.updateVisibleRegion);
     this.scrollClassAdder.detach();
-  }
+  };
 }
+
+customElements.define('br-mode-1up', Mode1UpLit);
