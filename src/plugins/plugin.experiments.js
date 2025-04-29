6f524b31e481dfc0a525a861876e49886ac9af69
Drini Cami <cdrini@gmail.com>
2025-04-28 22:19:14 -0400

    Add learn more link to experiments

--- src/plugins/plugin.experiments.js
+++ src/plugins/plugin.experiments.js
@@ -8,7 +8,7 @@ import { sleep } from '../BookReader/utils.js';
 const BookReader = /** @type {typeof import('@/src/BookReader.js').default} */(window.BookReader);
 
 class ExperimentModel {
-  /** @type {string} */
+  /** @type {string} test */
   name;
   /** @type {string} */
   description;
@@ -16,6 +16,8 @@ class ExperimentModel {
   icon;
   /** @type {boolean} */
   enabled;
+  /** @type {string} */
+  learnMore;
 
   enabledLoading = false;
 
@@ -35,7 +37,8 @@ export class ExperimentsPlugin extends BookReaderPlugin {
   experiments = [
     new class extends ExperimentModel {
       name = 'Hypothes.is';
-      description = `Publicly or privately annotate books and the web. Requires a Hypothes.is account.`;
+      description = 'Create public, collaborative, or fully private annotations on books and the web.';
+      learnMore = 'https://web.hypothes.is/about/';
       icon = 'https://web.hypothes.is/favicon.ico';
       enabled = false;
 
@@ -83,6 +86,7 @@ export class ExperimentsPlugin extends BookReaderPlugin {
   _render() {
     this.br.shell.menuProviders['experiments'] = {
       id: 'experiments',
+      // https://icon-sets.iconify.design/hugeicons/?icon-filter=eco-lab-02&query=lab
       icon: html`
         <svg xmlns="http://www.w3.org/2000/svg" width="34" viewBox="0 0 24 24"><path fill="currentColor" d="M10 5.75h1.25v2.5H9.5c-.41 0-.75.34-.75.75s.34.75.75.75h.25v1.96A5.72 5.72 0 0 0 6.25 17c0 3.17 2.58 5.75 5.75 5.75s5.75-2.58 5.75-5.75c0-2.33-1.39-4.4-3.5-5.29V9.75h.25c.41 0 .75-.34.75-.75s-.34-.75-.75-.75h-1.75v-1.5H14c1.52 0 2.75-1.23 2.75-2.75V3c0-.41-.34-.75-.75-.75h-2c-.579 0-1.115.178-1.558.483A2.75 2.75 0 0 0 10 1.25H8c-.41 0-.75.34-.75.75v1c0 1.52 1.23 2.75 2.75 2.75m2.75-.5V5c0-.69.56-1.25 1.25-1.25h1.25V4c0 .69-.56 1.25-1.25 1.25zm-1.5 6.98V9.75h1.5v2.48c0 .33.22.62.53.72c1.77.56 2.97 2.19 2.97 4.06A4.26 4.26 0 0 1 12 21.26a4.26 4.26 0 0 1-4.25-4.25c0-1.87 1.19-3.5 2.97-4.06c.32-.1.53-.39.53-.72m-2.5-9.48H10c.69 0 1.25.56 1.25 1.25v.25H10c-.69 0-1.25-.56-1.25-1.25z" color="currentColor"/></svg>
       `,
@@ -115,6 +119,7 @@ export class BrExperimentsPanel extends LitElement {
           .description="${experiment.description}"
           .enabled="${experiment.enabled}"
           .loading="${experiment.enabledLoading}"
+          .learnMore="${experiment.learnMore}"
           @toggle="${e => this._onToggleExperiment(e, experiment)}"
         ></br-experiment-toggle>
       `,
@@ -156,6 +161,7 @@ export class BrExperimentToggle extends LitElement {
   @property({ type: String }) description = '';
   @property({ type: Boolean }) enabled = false;
   @property({ type: Boolean }) loading = false;
+  @property({ type: String }) learnMore = '';
 
   /**
    * We want to disable the button immediately if loading, but only display
@@ -187,7 +193,10 @@ export class BrExperimentToggle extends LitElement {
           <img src="${this.icon}" style="width: 20px; height: 20px;" alt="" />
           <div style="flex-grow: 1; font-weight: bold;">${this.title}</div>
         </div>
-        <p style="font-size: 0.95em; opacity: 0.9">${this.description}</p>
+        <p style="opacity: 0.9">
+          ${this.description}
+          <a href="${this.learnMore}" target="_blank">Learn more</a>.
+        </p>
         <div style="display: flex">
           <div style="flex-grow: 1;"></div>
           <button @click="${this._toggleEnabled}" .disabled="${this.loading}">
