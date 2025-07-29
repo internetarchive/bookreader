8cf05b580e275f96ccd8939781d2aecd66ae94e1
Sandy Chu <sandy.chu.40@gmail.com>
2025-07-28 20:38:50 -0700

    Fix tts/utils.js ISO6391 issue

--- src/plugins/tts/utils.js
+++ src/plugins/tts/utils.js
@@ -53,12 +53,12 @@ export function toISO6391(language) {
  * @return {ISO6391?}
  */
 function searchForISO6391(language, columnsToSearch) {
-  for (let i = 0; i < langs.length; i++) {
+  for (let i = 0; i < langs.default.length; i++) {
     for (let colI = 0; colI < columnsToSearch.length; colI++) {
       const column = columnsToSearch[colI];
-      const columnValue = langs[i][COLUMN_TO_LANG_INDEX[column]];
+      const columnValue = langs.default[i][COLUMN_TO_LANG_INDEX[column]];
       if (columnValue.split(', ').map(x => x.toLowerCase()).indexOf(language) != -1) {
-        return langs[i][COLUMN_TO_LANG_INDEX['ISO 639-1']];
+        return langs.default[i][COLUMN_TO_LANG_INDEX['ISO 639-1']];
       }
     }
   }
